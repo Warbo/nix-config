b85f1d4c82a151878b1e251c19df8aea0e6d909c
Chris Warburton <chriswarbo@gmail.com>
2015-02-04 19:33:26 +0000

    Added all custom packages

--- config.nix
+++ config.nix
@@ -1,13 +1,18 @@
 {
   packageOverrides = pkgs: rec {
-    panpipe   = pkgs.callPackage ./local/panpipe.nix {};
-    panhandle = pkgs.callPackage ./local/panhandle.nix {};
+    # Custom packages
+    panpipe        = pkgs.callPackage ./local/panpipe.nix        {};
+    panhandle      = pkgs.callPackage ./local/panhandle.nix      {};
+    whitey         = pkgs.callPackage ./local/whitey.nix         {};
+    ml4pg          = pkgs.callPackage ./local/ml4pg.nix          {};
+    bugseverywhere = pkgs.callPackage ./local/bugseverywhere.nix {};
+    pidetop        = pkgs.callPackage ./local/pidetop.nix        {};
 
+    # Default Haskell modules
     hsEnv = pkgs.haskellPackages.ghcWithPackagesOld (pkgs : [
        pkgs.xmonad
        pkgs.xmonadExtras
        pkgs.xmonadContrib
-       # add more packages here
     ]);
   };
 }
