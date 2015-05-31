267c6f328798a19b9e193e588c3c71e3bb141127
Chris Warburton <chriswarbo@gmail.com>
2015-05-31 13:08:59 +0100

    Remove unnecessary Haskell modules

--- local/transformers.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, stdenv }:
-mkDerivation {
-  pname = "transformers";
-  version = "0.4.3.0";
-  sha256 = "179sbhvc9dghyw58hz80109pbrzgh7vh437227a51jhmx2bsgl5k";
-  buildDepends = [ base ];
-  description = "Concrete functor and monad transformers";
-  license = stdenv.lib.licenses.bsd3;
-}
