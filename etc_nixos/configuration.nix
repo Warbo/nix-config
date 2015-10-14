4b3c3560ae1af80f70cfa613911f0273bb3d1741
Chris Warburton <chriswarbo@gmail.com>
2015-10-14 16:54:00 +0100

    Changes

--- etc_nixos/configuration.nix
+++ etc_nixos/configuration.nix
@@ -20,20 +20,14 @@ rec {
     kernelModules = [ "kvm-intel" "tun" "virtio" ];
     kernel.sysctl."net.ipv4.tcp_sack" = 0;
   };
+
   networking = {
     hostName                = "nixos";
     interfaceMonitor.enable = false; # Watch for plugged cable.
     firewall.enable         = false;
 
     # Block time wasters
-    extraHosts = ''
-      127.0.0.1 news.ycombinator.com
-      127.0.0.1 slashdot.org
-      127.0.0.1 reddit.com
-      127.0.0.1 tumblr.com
-      127.0.0.1 4chan.org
-      127.0.0.1 news.bbc.co.uk
-    '';
+    extraHosts = builtins.readFile "/home/chris/.dotfiles/hosts";
 
     # NetworkManager
     networkmanager.enable = true;
@@ -55,7 +49,15 @@ rec {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
-    trayer networkmanagerapplet pmutils
+    trayer networkmanagerapplet pmutils shared_mime_info
+    # KDE
+    kde4.kdemultimedia kde4.kdegraphics kde4.kdeutils kde4.applications
+    kde4.kdegames kde4.kdeedu kde4.kdebindings kde4.kdeaccessibility
+    kde4.kde_baseapps kde4.kactivities kde4.kdeadmin kde4.kdeartwork
+    kde4.kde_base_artwork kde4.kdenetwork kde4.kdepim kde4.kdepimlibs
+    kde4.kdeplasma_addons kde4.kdesdk kde4.kdetoys kde4.kde_wallpapers
+    kde4.kdewebdev kde4.oxygen_icons kde4.kdebase_workspace kde4.kdelibs
+    kde4.kdevelop kde4.kdevplatform kde4.kbibtex
   ];
 
   # List services that you want to enable:
@@ -64,6 +66,8 @@ rec {
   services.openssh.enable = true;
 
   environment.etc."ssh/ssh_config".source = /home/chris/ssh_config;
+
+  #environment.etc."mime.types".source = /home/chris/.dotfiles/mime.types;
 
   # Enable the X11 windowing system.
   services.xserver = {
@@ -79,7 +83,13 @@ rec {
       };
     };
 
-    desktopManager.default = "none";
+    desktopManager = {
+      default = "none";
+      kde4 = {
+        enable = true;
+      };
+    };
+
     displayManager = {
       auto = {
         enable = true;
@@ -95,20 +105,39 @@ rec {
     extraFlags = ["--prunepaths='/home/chris/Public /home/chris/Uni'"];
   };
 
+  services.printing = {
+    enable  = true;
+    drivers = [ pkgs.gutenprint ];
+  };
+
+  services.atd = {
+    enable = true;
+  };
+
   # Locale, etc.
   i18n = {
     defaultLocale = "en_GB.UTF-8";
-    consoleKeyMap = "gb";
+    consoleKeyMap = "uk";
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.chris = {
     name        = "chris";
     group       = "users";
-    extraGroups = [ "wheel" "voice" "networkmanager" "fuse" "dialout" ];
+    extraGroups = [ "wheel" "voice" "networkmanager" "fuse" "dialout" "atd" ];
     uid         = 1000;
     createHome  = true;
     home        = "/home/chris";
+    shell       = "/run/current-system/sw/bin/bash";
+  };
+
+  users.extraUsers.kde = {
+    name        = "kde";
+    group       = "users";
+    extraGroups = [ "wheel" "voice" "networkmanager" "fuse" "dialout" "atd" ];
+    uid         = 1001;
+    createHome  = true;
+    home        = "/home/kde";
     shell       = "/run/current-system/sw/bin/bash";
   };
 }
