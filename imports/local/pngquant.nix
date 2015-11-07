2099dd6bf8815d5a54207baf69a44f9d49fe4527
Chris Warburton <chriswarbo@gmail.com>
2015-11-07 01:23:56 +0000

    Discard pngquant

--- imports/local/pngquant.nix
+++
@@ -1,9 +0,0 @@
-{ stdenv, latestGit, libpng, which, zlib }:
-
-stdenv.mkDerivation {
-  name = "pngquant";
-
-  src = latestGit { url = git://github.com/pornel/pngquant.git; };
-
-  buildInputs = [ libpng which zlib ];
-}
