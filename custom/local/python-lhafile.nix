787776f698e246ff0f89dd4cf7925250e1e8c2e0
Chris Warburton <chriswarbo@gmail.com>
2015-12-21 22:05:40 +0000

    LhaFile for FS-UAE

---
+++ custom/local/python-lhafile.nix
@@ -0,0 +1,15 @@
+{ fetchurl, pythonPackages, buildPythonPackage }:
+
+buildPythonPackage {
+  name = "python-lhafile";
+  version = "0.2.1";
+
+  src = fetchurl {
+    url = http://fengestad.no/python-lhafile/python-lhafile-0.2.1.tar.gz;
+    sha256 = "0pp30hkxrfx1qnrdzr40sm0y7h7w5vlwa0m7kwali2nh29rzmpfc";
+  };
+
+  propagatedBuildInputs = [
+    pythonPackages.python
+  ];
+}
