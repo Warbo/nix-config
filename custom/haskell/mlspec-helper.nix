2fcc78310f4067bb6f109570f6fdb7d135c1a60f
Chris Warburton <chriswarbo@gmail.com>
2016-02-04 15:36:05 +0000

    Use online repo for ml4hs-helper

--- custom/haskell/mlspec-helper.nix
+++ custom/haskell/mlspec-helper.nix
@@ -1,4 +1,3 @@
 (import <nixpkgs> {}).haskellGit {
-  #http://chriswarbo.net/git/mlspec-helper.git
-  url = "/home/chris/Programming/repos/mlspec-helper.git";
+  url = http://chriswarbo.net/git/mlspec-helper.git;
 }
