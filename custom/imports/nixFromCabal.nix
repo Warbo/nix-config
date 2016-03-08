002cb621574dd86ff9e0fcb149152f54ec0036f7
Chris Warburton <chriswarbo@gmail.com>
2016-03-08 14:21:22 +0000

    Undo unnecessary hack

--- custom/imports/nixFromCabal.nix
+++ custom/imports/nixFromCabal.nix
@@ -20,12 +20,9 @@ assert f == null || isFunction f;
 
 let pkgs  = import <nixpkgs> {};
     hsVer = pkgs.haskellPackages.ghc.version;
-    inGit     = elem ".git" (builtins.attrNames (builtins.readDir ./..));
-    sanitise  = with pkgs.lib.strings; stringAsChars (c: if elem c (lowerChars ++ upperChars ++ stringToCharacters "0123456789") then c else "");
-    extraHash = sanitise (if inGit then readFile ../.git/refs/heads/master else "");
     nixed = pkgs.stdenv.mkDerivation {
       inherit dir;
-      name         = "nixFromCabal-${hsVer}${extraHash}";
+      name         = "nixFromCabal-${hsVer}";
       buildInputs  = [ pkgs.haskellPackages.cabal2nix ];
       buildCommand = ''
         source $stdenv/setup
