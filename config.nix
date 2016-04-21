d357529dc5167ec6cd4432613222ded204489203
Chris Warburton <chriswarbo@gmail.com>
2016-04-21 15:04:44 +0100

    Remove rendundant strip/add

--- config.nix
+++ config.nix
@@ -4,7 +4,7 @@
     with pkgs; with lib;
     let mkPkg = x: old:
        let n = removeSuffix ".nix" x;
-        in old // (import (./custom + "/${n}.nix") pkgs);
+        in old // (import (./custom + "/${x}") pkgs);
      in fold mkPkg
              {}
              (filter (hasSuffix ".nix")
