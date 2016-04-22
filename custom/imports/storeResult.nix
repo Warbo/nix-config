1cac91ed84c0ea66d045228b732dd2290544f7d8
Chris Warburton <chriswarbo@gmail.com>
2016-04-22 16:00:34 +0100

    Move storeResult script out of local, as it's not a package

--- custom/imports/storeResult.nix
+++ custom/imports/storeResult.nix
@@ -1,7 +1,9 @@
-{ writeScript }:
+with import <nixpkgs> {};
 
 # A script we can embed in others. Adds its first arg to the Nix store, and
 # writes the resulting path to the second arg. This avoids newline issues.
+# Make sure `nix` is in your buildInputs (or equivalent), and the relevant
+# environment variables are set.
 writeScript "store-result" ''
   set -e
   RESULT=$(nix-store --add "$1")
