07977304afe24576b8b59995cc5e264600dca0e3
Chris Warburton <chriswarbo@gmail.com>
2016-04-22 15:06:47 +0100

    Remove obsolete package

--- custom/local/explore-theories.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, withLatestGit }:
-
-withLatestGit {
-  url      =  if (import <nixpkgs> {}).localOnly
-                 then /home/chris/Programming/repos/explore-theories.git
-                 else http://chriswarbo.net/git/explore-theories.git;
-  srcToPkg = (x: pkgs.callPackage "${x}" {});
-}
