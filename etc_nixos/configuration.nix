12520509e5def20867d3b33e4ecdc30dfafba96a
Chris Warburton <chriswarbo@gmail.com>
2016-04-22 13:49:36 +0100

    Disable MESA fix, unless things are too unstable

--- etc_nixos/configuration.nix
+++ etc_nixos/configuration.nix
@@ -14,12 +14,12 @@ rec {
       # Workaround for https://github.com/NixOS/nixpkgs/issues/11467
       # Build mesa_drivers with llvm_36.
       # The r600 driver doesn't work with llvm_37.
-      mesa_drivers = self.mesaDarwinOr (
-        let mo = self.mesa_noglu.override {
-          llvmPackages = self.llvmPackages_36;
-        };
-         in mo.drivers
-      );
+      #mesa_drivers = self.mesaDarwinOr (
+      #  let mo = self.mesa_noglu.override {
+      #    llvmPackages = self.llvmPackages_36;
+      #  };
+      #   in mo.drivers
+      #);
     };
   };
 
