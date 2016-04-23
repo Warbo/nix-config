893fe336ba0d9bf6244075bd209318e1ab2c0663
Chris Warburton <chriswarbo@gmail.com>
2016-04-23 12:10:11 +0100

    Remove obsolete package

--- custom/haskell/TreeFeatures.nix
+++
@@ -1,5 +0,0 @@
-(import <nixpkgs> {}).haskellGit {
-  url = if (import <nixpkgs> {}).localOnly
-           then /home/chris/Programming/repos/tree-features.git
-           else http://chriswarbo.net/git/tree-features.git;
-}
--- custom/local/TreeFeatures.nix
+++
@@ -1,3 +0,0 @@
-{ haskellPackages }:
-
-haskellPackages.TreeFeatures
