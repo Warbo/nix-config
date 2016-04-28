df06adde0e038049d153cf485233457d32c9667b
Chris Warburton <chriswarbo@gmail.com>
2016-04-28 02:37:58 +0100

    Fix runWeka

--- custom/local/runWeka.nix
+++ custom/local/runWeka.nix
@@ -1,7 +1,6 @@
-{ latestGit }:
-
-import (latestGit {
-          url = if (import <nixpkgs> {}).localOnly
-                   then "/home/chris/Programming/repos/run-weka.git"
-                   else http://chriswarbo.net/git/run-weka.git;
-        })
+let pkgs = import <nixpkgs> {};
+ in import (pkgs.latestGit {
+      url = if pkgs.localOnly
+               then "/home/chris/Programming/repos/run-weka.git"
+               else http://chriswarbo.net/git/run-weka.git;
+    })
