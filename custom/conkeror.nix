48b279160b0e914e2ca940a7676514f742072ca7
Chris Warburton <chriswarbo@gmail.com>
2016-06-14 11:59:23 +0100

    Latest conkeror

---
+++ custom/conkeror.nix
@@ -0,0 +1,25 @@
+pkgs:
+
+with import <nixpkgs> {};
+
+{
+  conkeror = stdenv.mkDerivation rec {
+    pkgname = "conkeror";
+    version = "git";
+    name    = "${pkgname}-${version}";
+
+    src = latestGit {
+            url = git://repo.or.cz/conkeror.git;
+          };
+
+    buildInputs = [ unzip makeWrapper ];
+
+    installPhase = ''
+      mkdir -p $out/libexec/conkeror
+      cp -r * $out/libexec/conkeror
+
+      makeWrapper ${firefox}/bin/firefox $out/bin/conkeror \
+        --add-flags "-app $out/libexec/conkeror/application.ini"
+    '';
+  };
+}
