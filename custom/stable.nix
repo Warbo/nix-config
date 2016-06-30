6a2ea65ceea9b2bed75601b956c315dd39196596
Chris Warburton <chriswarbo@gmail.com>
2016-06-30 11:54:33 +0100

    Explicitly avoid our overrides for stable nixpkgs

--- custom/stable.nix
+++ custom/stable.nix
@@ -1,7 +1,14 @@
 # Stable nixpkgs
-import ((import <nixpkgs> {}).fetchFromGitHub {
-  owner  = "NixOS";
-  repo   = "nixpkgs";
-  rev    = "16.03";
-  sha256 = "0m2b5ignccc5i5cyydhgcgbyl8bqip4dz32gw0c6761pd4kgw56v";
-}) {}
+pkgs:
+
+let repo = pkgs.fetchFromGitHub {
+             owner  = "NixOS";
+             repo   = "nixpkgs";
+             rev    = "16.03";
+             sha256 = "0m2b5ignccc5i5cyydhgcgbyl8bqip4dz32gw0c6761pd4kgw56v";
+           };
+in {
+  # Explicitly pass an empty config, to avoid loading ~/.nixpkgs/config.nix and
+  # causing an infinite loop
+  stable = import repo { config = {}; };
+}
