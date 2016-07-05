24e391077955addfeaeead3573440363a2750083
Chris Warburton <chriswarbo@gmail.com>
2016-07-05 21:57:51 +0100

    put each package in release.nix separately

--- release.nix
+++ release.nix
@@ -5,22 +5,27 @@ in with builtins;
    with (pkgs // overrides);
    with lib;
 
-{
+let haskell = let broken  = [ "ghc6123" ];
+                  working = filterAttrs (n: _: !(elem n broken))
+                                        overrides.haskell.packages;
+                  all = mapAttrs
+                          (version: pkgs:
+                             listToAttrs (map (p: {
+                                                name  = "${p}-${version}";
+                                                value = trace "Version ${version}" pkgs."${p}";
+                                              })
+                                              overrides.haskellNames))
+                          working;
+                  lst = fold (version: rest:
+                         fold (pkg: rest:
+                                rest ++ [{
+                                           name  = "haskell-${pkg}";
+                                           value = all."${version}"."${pkg}";
+                                        }])
+                              rest
+                              (attrNames all."${version}"))
+                       []
+                       (attrNames all);
+               in listToAttrs lst;
 
- haskell = buildEnv {
-   name  = "custom-haskell";
-   paths = let broken  = [ "ghc6123" ];
-               working = filterAttrs (n: _: !(elem n broken))
-                                     overrides.haskell.packages;
-               all = mapAttrs
-                       (version: pkgs:
-                          listToAttrs (map (p: {
-                                             name  = "${p}-${version}";
-                                             value = trace "Version ${version}" pkgs."${p}";
-                                           })
-                                           overrides.haskellNames))
-                       working;
-            in concatMap attrValues (attrValues all);
- };
-
-}
+in haskell
