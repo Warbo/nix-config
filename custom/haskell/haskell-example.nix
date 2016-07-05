a9f23289f5545282081a451652d64e1559a660b2
Chris Warburton <chriswarbo@gmail.com>
2016-07-05 19:33:47 +0100

    Don't rely on /home/chris

--- custom/haskell/haskell-example.nix
+++ custom/haskell/haskell-example.nix
@@ -1,4 +1,17 @@
 self: super: with self;
 
-nixFromCabal /home/chris/Writing/TransferReport/haskell_example
-             null
+let remote = latestGit {
+               url = http://chriswarbo.net/git/writing.git;
+             };
+    online = stdenv.mkDerivation {
+               name = "haskell-example-src";
+               src  = remote;
+               buildCommand = ''
+                 source $stdenv/setup
+
+                 cp -ar "$src/TransferReport/haskell_example" "$out"
+               '';
+             };
+    local  = "/home/chris/Writing/TransferReport/haskell_example";
+    src    = onOff online local;
+ in nixFromCabal src null
