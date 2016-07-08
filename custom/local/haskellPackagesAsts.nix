3b76387ab23d8b66bfc1ca9ec294734e20c07931
Chris Warburton <chriswarbo@gmail.com>
2016-07-08 12:53:40 +0100

    Remove unused stuff

--- custom/local/haskellPackagesAsts.nix
+++
@@ -1,7 +0,0 @@
-{ ghcast, callPackage }:
-
-let inPkgs = s: with builtins; toPath (toString <nixpkgs> + "/pkgs/" + s);
- in callPackage (inPkgs "development/haskell-modules") {
-      ghc = ghcast;
-      compilerConfig = callPackage (inPkgs "development/haskell-modules/configuration-ghc-7.10.x.nix") {};
-    }
