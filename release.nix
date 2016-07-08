84380dd758981a3c8611c362ed362bcf95d4d173
Chris Warburton <chriswarbo@gmail.com>
2016-07-08 11:13:09 +0100

    Simplify release.nix

--- release.nix
+++ release.nix
@@ -6,18 +6,16 @@ in with builtins;
    with lib;
 
 let dbg     = x: trace (toJSON x) x;
-    haskell = let selectedVersions = filterAttrs (n: _: elem n [ "ghc784" ])
+    haskell = let selectedVersions = filterAttrs (version: _: elem version [ "ghc784" ])
                                                  overrides.haskell.packages;
                   selectedPkgs     = mapAttrs (version:
                                                 filterAttrs (name: _:
                                                               elem name overrides.haskellNames))
                                               selectedVersions;
 
-                  prefixed         = mapAttrs (version: hsPkgs:
-                                                mapAttrs' (name: pkg:
-                                                            nameValuePair ("haskell-${name}-${version}")
-                                                                          pkg)
-                                                          hsPkgs)
+                  prefixed         = mapAttrs (version:
+                                                mapAttrs' (name:
+                                                            nameValuePair ("haskell-${name}-${version}")))
                                               selectedPkgs;
                in fold (x: y: x // y) {} (attrValues prefixed);
 
