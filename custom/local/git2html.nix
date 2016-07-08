cfc0f14e36b8f0788fec376bfe6e13c0a770938a
Chris Warburton <chriswarbo@gmail.com>
2016-07-08 13:52:23 +0100

    Typo

--- custom/local/git2html.nix
+++ custom/local/git2html.nix
@@ -4,4 +4,5 @@ stdenv.lib.overrideDerivation git2html-real (old: {
   src = latestGit {
     url = onOff http://chriswarbo.net/git/git2html.git
                 /home/chris/Programming/git2html;
+  };
 })
