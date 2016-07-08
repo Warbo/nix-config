a61d2d09050a868e4c364fca3b7d48335f39a68c
Chris Warburton <chriswarbo@gmail.com>
2016-07-08 11:29:36 +0100

    Simplify release.nix

--- release.nix
+++ release.nix
@@ -1,20 +1,18 @@
 # Used for testing and building via Hydra or "nix-build"
-let pkgs = import <nixpkgs> { config = import ./config.nix; };
-in with builtins;
-   with pkgs;
-   with lib;
+with import <nixpkgs> { config = import ./config.nix; };
+with builtins;
+with lib;
 
-let dbg     = x: trace (toJSON x) x;
-    haskell = let selectedVersions = filterAttrs (version: _: !(hasPrefix "lts" version))
-                                                 pkgs.haskell.packages;
+let haskellPkgs = let selectedVersions = filterAttrs (version: _: !(hasPrefix "lts" version))
+                                                     haskell.packages;
 
-                  prefixed         = mapAttrs (version: hsPkgs:
-                                                mapAttrs' (name:
-                                                            nameValuePair ("haskell-${name}-${version}"))
-                                                          (filterAttrs (name: _:
-                                                                         elem name pkgs.haskellNames)
-                                                                       hsPkgs))
-                                              selectedVersions;
-               in fold (x: y: x // y) {} (attrValues prefixed);
+                      prefixed         = mapAttrs (version: hsPkgs:
+                                                    mapAttrs' (name:
+                                                                nameValuePair ("haskell-${name}-${version}"))
+                                                              (filterAttrs (name: _:
+                                                                             elem name haskellNames)
+                                                                           hsPkgs))
+                                                  selectedVersions;
+                   in fold (x: y: x // y) {} (attrValues prefixed);
 
-in haskell
+in haskellPkgs
