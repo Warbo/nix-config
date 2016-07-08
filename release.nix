b7c4039a29c28d81b4fb2b8407801a226767128f
Chris Warburton <chriswarbo@gmail.com>
2016-07-08 11:27:20 +0100

    Removed redundant 'overrides'

--- release.nix
+++ release.nix
@@ -1,19 +1,18 @@
 # Used for testing and building via Hydra or "nix-build"
-let pkgs      = import <nixpkgs> { config = import ./other.nix; };
-    overrides = import ./custom.nix pkgs;
+let pkgs = import <nixpkgs> { config = import ./config.nix; };
 in with builtins;
-   with (pkgs // overrides);
+   with pkgs;
    with lib;
 
 let dbg     = x: trace (toJSON x) x;
     haskell = let selectedVersions = filterAttrs (version: _: !(hasPrefix "lts" version))
-                                                 overrides.haskell.packages;
+                                                 pkgs.haskell.packages;
 
                   prefixed         = mapAttrs (version: hsPkgs:
                                                 mapAttrs' (name:
                                                             nameValuePair ("haskell-${name}-${version}"))
                                                           (filterAttrs (name: _:
-                                                                         elem name overrides.haskellNames)
+                                                                         elem name pkgs.haskellNames)
                                                                        hsPkgs))
                                               selectedVersions;
                in fold (x: y: x // y) {} (attrValues prefixed);
