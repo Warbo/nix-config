525b5f5c876589607ebe3f02e21a4cf814dd66bb
Chris Warburton <chriswarbo@gmail.com>
2016-08-17 17:29:31 +0100

    Remove xpra

--- custom/xpra.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-
-let fixedPkgSrc = self.latestGit {
-      url = "https://github.com/abbradar/nixpkgs.git";
-      ref = "xpra";
-    };
-    fixedPkgs = import "${fixedPkgSrc}" { config = x: {}; };
- in { inherit (fixedPkgs) xpra; }
