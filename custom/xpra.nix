cdbb7c2527fbd934261e996b29a5fc9214fe1b19
Chris Warburton <chriswarbo@gmail.com>
2016-08-17 16:26:01 +0100

    xpra

--- custom/xpra.nix
+++ custom/xpra.nix
@@ -1,8 +1,10 @@
 self: super:
 
+/*
 let fixedPkgSrc = self.latestGit {
       url = "https://github.com/abbradar/nixpkgs.git";
       ref = "xpra";
     };
     fixedPkgs = import "${fixedPkgSrc}" { config = x: {}; };
- in { inherit (fixedPkgs) xpra; }
+ in*/
+{ inherit (self.unstable) xpra; }
