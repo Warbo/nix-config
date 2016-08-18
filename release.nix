1f5b846bb7f75b951170651abe07b416899c0cfa
Chris Warburton <chriswarbo@gmail.com>
2016-08-18 14:21:11 +0100

    Don't bother mixing sets

--- release.nix
+++ release.nix
@@ -18,30 +18,18 @@ customNames = filter (n: !(elem n [
                          pkgs."${n}".type == "derivation")
                      customPkgNames;
 
-topLevel = fold (name: rest: rest // { "${name}" = pkgs."${name}"; })
-                {}
-                customNames;
+topLevel = listToAttrs (map (name: {
+                              inherit name;
+                              value = pkgs."${name}";
+                            })
+                            customNames);
 
 # Select our custom Haskell packages from the various sets of Haskell packages
 # provided by nixpkgs (e.g. for different compiler versions)
-haskellPkgs = let
-  selectedSets = {
-                   inherit haskellPackages profiledHaskellPackages;
-                   inherit (haskell.packages) ghc7103 ghc801 lts;
-                   stable = stable.haskellPackages;
-                 };
-
-  # Give our packages unique names, so different sets won't overlap
-  suffixed     = mapAttrs (set: hsPkgs:
-                            # Rename the package attributes for this set
-                            mapAttrs' (name:
-                                         nameValuePair ("haskell-${name}-${set}"))
-                                      # Only select our custom packages
-                                      (filterAttrs (name: _:
-                                                     elem name haskellNames)
-                                                   hsPkgs))
-                          selectedSets;
-  # Combine all of the selected packages for all of the selected sets
-  in fold (x: y: x // y) {} (attrValues suffixed);
+haskellPkgs = mapAttrs (set: hsPkgs:
+                         filterAttrs (name: _: elem name haskellNames) hsPkgs)
+                       { inherit haskellPackages profiledHaskellPackages;
+                         inherit (haskell.packages) ghc7103 ghc801 lts;
+                         stable = stable.haskellPackages; };
 
 in topLevel // haskellPkgs
