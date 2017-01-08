2c8a763025c4750f68a0f00f2cd4a64e5a442957
Chris Warburton <chriswarbo@gmail.com>
2017-01-08 05:23:47 +0000

    Add explicit fallback repo

--- custom/haskell/structural-induction.nix
+++ custom/haskell/structural-induction.nix
@@ -4,7 +4,7 @@ with self;
 import (runCommand "get" {
     buildInputs = [ cabal2nix cabal-install nix-prefetch-scripts nix ];
     NIX_REMOTE  = "daemon";
-    NIX_PATH    = builtins.getEnv "NIX_PATH";
+    NIX_PATH    = builtins.getEnv "NIX_PATH" + ":${stableRepo}";
   }
   ''
     export HOME="$PWD"
