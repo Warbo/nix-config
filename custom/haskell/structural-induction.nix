f4f99409f1d6cd2f499f944125ae084d167ad47c
Chris Warburton <chriswarbo@gmail.com>
2017-01-08 05:35:14 +0000

    Hacky fix

--- custom/haskell/structural-induction.nix
+++ custom/haskell/structural-induction.nix
@@ -1,10 +1,16 @@
 self: super:
 
 with self;
+with {
+  repoHack = runCommand "repo-hack" {} ''
+    mkdir -p "$out"
+    cp -r "${stableRepo}" "$out/nixpkgs"
+  '';
+};
 import (runCommand "get" {
     buildInputs = [ cabal2nix cabal-install nix-prefetch-scripts nix ];
     NIX_REMOTE  = "daemon";
-    NIX_PATH    = builtins.getEnv "NIX_PATH" + ":${stableRepo}";
+    NIX_PATH    = builtins.getEnv "NIX_PATH" + ":${repoHack}";
   }
   ''
     export HOME="$PWD"
