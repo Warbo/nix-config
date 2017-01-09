1ef7453b607c381a2d12afe238bc3a321bbebade
Chris Warburton <chriswarbo@gmail.com>
2017-01-09 12:59:11 +0000

    Simplify

--- etc_nixos/services.nix
+++ etc_nixos/services.nix
@@ -2,7 +2,8 @@ with builtins;
 
 pkgs: with pkgs;
 
-let mkService = opts: {
+with rec {
+  mkService = opts: {
         enable   = true;
         wantedBy = [ "default.target"  ];
         after    = [ "local-fs.target" ];
@@ -11,53 +12,65 @@ let mkService = opts: {
             Type = "simple";
           } // opts.serviceConfig;
         };
-in {
-  emacs =
-    let sudoWrapper = stdenv.mkDerivation {
-          name = "sudo-wrapper";
-          buildCommand = ''
-            mkdir -p "$out/bin"
-            ln -s /var/setuid-wrappers/sudo "$out/bin/sudo"
-          '';
-        };
-     in mkService {
-          description     = "Emacs daemon";
-          path            = [ all sudoWrapper mu ];
-          environment     = { SSH_AUTH_SOCK = "/run/user/1000/ssh-agent"; };
-          reloadIfChanged = true;  # As opposed to restarting
-          serviceConfig   = {
-            User       = "chris";
-            Type       = "forking";
-            Restart    = "always";
-            ExecStart  = ''"${emacs}/bin/emacs" --daemon'';
-            ExecStop   = ''"${emacs}/bin/emacsclient" --eval "(kill-emacs)"'';
-            ExecReload = ''"${emacs}/bin/emacsclient" --eval "(load-file \"~/.emacs.d/init.el\")"'';
-          };
-        };
+
+  sudoWrapper = stdenv.mkDerivation {
+    name = "sudo-wrapper";
+    buildCommand = ''
+      mkdir -p "$out/bin"
+      ln -s /var/setuid-wrappers/sudo "$out/bin/sudo"
+    '';
+  };
+
+  pingWrapper = stdenv.mkDerivation {
+    name = "ping-wrapper";
+    buildCommand = ''
+      mkdir -p "$out/bin"
+      ln -s /var/setuid-wrappers/ping "$out/bin/ping"
+    '';
+  };
+};
+{
+  emacs = mkService {
+    description     = "Emacs daemon";
+    path            = [ all sudoWrapper mu ];
+    environment     = { SSH_AUTH_SOCK = "/run/user/1000/ssh-agent"; };
+    reloadIfChanged = true;  # As opposed to restarting
+    serviceConfig   = {
+      User       = "chris";
+      Type       = "forking";
+      Restart    = "always";
+      ExecStart  = ''"${emacs}/bin/emacs" --daemon'';
+      ExecStop   = ''"${emacs}/bin/emacsclient" --eval "(kill-emacs)"'';
+      ExecReload = ''"${emacs}/bin/emacsclient" --eval "(load-file \"~/.emacs.d/init.el\")"'';
+    };
+  };
 
   shell = mkService {
     description     = "Long-running terminal multiplexer";
     path            = [ dvtm dtach ];
     environment     = {
       DISPLAY = ":0";
-      TERM    = "xterm";
+      TERM    = "xterm"; # Useful when remote servers don't have dvtm
     };
     reloadIfChanged = true;  # As opposed to restarting
     serviceConfig   = {
       User      = "chris";
       Restart   = "always";
-      ExecStart =
-        let session = writeScript "session" ''
-              #!${bash}/bin/bash
-              exec dvtm -M -m ^b
-            '';
-         in writeScript "shell-start" ''
-              #!${bash}/bin/bash
-              cd
-              exec dtach -A ~/.sesh -r winch "${session}"
-            '';
+      ExecStart = writeScript "shell-start" ''
+        #!${bash}/bin/bash
+        cd
+        exec dtach -A ~/.sesh -r winch "${writeScript "session" ''
+          #!${bash}/bin/bash
+          exec dvtm -M -m ^b
+        ''}"
+      '';
+
+      # Don't stop, since it may kill programs we want to keep using
       ExecStop   = ''"${coreutils}/bin/true"'';
       ExecReload = ''"${coreutils}/bin/true"'';
+
+      # Since we run at startup, X might not be up; pretend we're on a virtual
+      # terminal (e.g. ctrl-alt-f6) for the purpose of DVTM's capability queries
       StandardInput  = "tty";
       StandardOutput = "tty";
       TTYPath        = "/dev/tty6";
@@ -66,7 +79,7 @@ in {
 
   hometime = mkService {
     description = "Count down to the end of the work day";
-    path        = with pkgs; [ gksu libnotify iputils networkmanager pmutils ];
+    path        = [ gksu libnotify iputils networkmanager pingWrapper pmutils ];
     environment = {
       DISPLAY    = ":0";
       XAUTHORITY = "/home/chris/.Xauthority";
@@ -75,61 +88,54 @@ in {
       User       = "chris";
       Restart    = "always";
       RestartSec = 300;
-      ExecStart  =
-        let location = writeScript "location" ''
-              #!${bash}/bin/bash
+      ExecStart  = writeScript "hometime" ''
+        #!${bash}/bin/bash
+        set -e
 
-              # If we're not online, we can't tell where we are
-              /var/setuid-wrappers/ping -c 1 google.com 1>/dev/null 2>/dev/null || {
-                echo "unknown"
-                exit 0
-              }
+        HOUR=$(date "+%H")
+        if [[ "$HOUR" -lt 17 ]]
+        then
+          echo "Too early to notify"
+          exit
+        fi
 
-              # The WiFi network we're on should tell us where we are
-              NET=$(nmcli c | grep -v -- "--" | grep -v "DEVICE" | cut -d ' ' -f1)
-              if [[ "x$NET" = "xaa.net.uk" ]]
-              then
-                echo "home"
-              elif [[ "x$NET" = "xUoD_WiFi" ]] || [[ "x$NET" = "xeduroam" ]]
-              then
-                echo "uni"
-              else
-                echo "unknown"
-              fi
-            '';
-       in writeScript "hometime" ''
-            #!${bash}/bin/bash
-            set -e
-            set -x
+        if ping -c 1 google.com 1>/dev/null 2>/dev/null
+        then
+          echo "We're online, getting location from WiFi network name"
+        else
+          echo "Can't get location from WiFi network (we're offline)"
+          exit
+        fi
 
-            # Set DBus variables to make notifications work
-            MID=$(cat /etc/machine-id)
-              D=$(echo "$DISPLAY" | cut -d '.' -f1 | tr -d :)
-            source ~/.dbus/session-bus/"$MID"-"$D"
-            export DBUS_SESSION_BUS_ADDRESS
+        NET=$(nmcli c | grep -v -- "--" | grep -v "DEVICE" | cut -d ' ' -f1)
+        if [[ "x$NET" = "xUoD_WiFi" ]] || [[ "x$NET" = "xeduroam" ]]
+        then
+          echo "We're at uni"
+        else
+          echo "Not at uni, so not notifying"
+          exit 1
+        fi
 
-            function notify {
-              notify-send -t 0 "Home Time" "$1"
-            }
+        # Set DBus variables to make notifications appear on X display
+        MID=$(cat /etc/machine-id)
+          D=$(echo "$DISPLAY" | cut -d '.' -f1 | tr -d :)
+        source ~/.dbus/session-bus/"$MID"-"$D"
+        export DBUS_SESSION_BUS_ADDRESS
 
-            LOC=$(${location})
-            if [[ "x$LOC" = "xuni" ]]
-            then
-              HOUR=$(date "+%H")
-              if [[ "$HOUR" -gt "16" ]]
-              then
-                notify "Past 5pm; half an hour until suspend"
-                sleep 600
-                notify "20 minutes until suspend"
-                sleep 600
-                notify "10 minutes until suspend"
-                sleep 600
-                notify "Suspending"
-                sleep 60
-                gksudo -S pm-suspend
-              fi
-            fi
-          '';
+        function notify {
+          notify-send -t 0 "Home Time" "$1"
+        }
+
+        notify "Past 5pm; half an hour until suspend"
+        sleep 600
+        notify "20 minutes until suspend"
+        sleep 600
+        notify "10 minutes until suspend"
+        sleep 600
+        notify "Suspending"
+        sleep 60
+        gksudo -S pm-suspend
+      '';
     };
   };
 
@@ -286,7 +292,7 @@ in {
         #!${bash}/bin/bash
         sshfs -f ${opts extraOptions} ${addr} ${dir}
       '';
-      ExecStop = writeScript "afuse-unmount" ''
+      ExecStop = writeScript "sshfuse-unmount" ''
         #!${bash}/bin/bash
         pkill -f -9 "sshfs.*${dir}"
         /var/setuid-wrappers/fusermount -u -z "${dir}"
@@ -317,7 +323,7 @@ in {
 
   pi-monitor = mkService {
     description = "Unmount raspberrypi when unreachable";
-    path = [ iputils ];
+    path = [ iputils utillinux ];
     serviceConfig = {
       User = "root";
       Restart = "always";
@@ -326,17 +332,19 @@ in {
         #!${bash}/bin/bash
         if /var/setuid-wrappers/ping -c 1 raspberrypi
         then
-          # We're home
+          # We're home, no need to unmount anything
           exit 0
-        done
+        fi
 
         # We're not home; check if raspberrypi is mounted
         if mount | grep raspberrypi
         then
           # Anything trying to access the mount will hang, making KILL the
           # only reliable way to un-hang processes
+          pkill -9 -f 'sshfs.*raspberrypi'
 
-          echo "TODO"
+          # Try to unmount more cleanly too
+          fusermount -u -z /home/chris/Public
         fi
       '';
     };
@@ -357,8 +365,8 @@ in {
         echo "Checking for existing bind"
         if pgrep -f 'ssh.*22222:localhost:22222'
         then
-          echo "Existing bind found, sleeping"
-          exit 0
+          echo "Existing bind found, aborting"
+          exit 1
         fi
 
         echo "No existing binds found, binding port"
@@ -371,7 +379,7 @@ in {
 
   desktop-monitor = mkService {
     description   = "Kill desktop-bind if it's hung";
-    path          = [ iputils openssh procps ];
+    path          = [ iputils openssh procps su.su ];
     environment   = { SSH_AUTH_SOCK = "/run/user/1000/ssh-agent"; };
     serviceConfig = {
       User       = "root";
@@ -384,15 +392,17 @@ in {
         if pgrep -f "$PAT"
         then
           # Bind is running, make sure it's working
-          if ssh -A localhost -p 22222
+          if su -c 'ssh -A user@localhost -p 22222 true' - chris
           then
             # Seems to be working
             true
           else
-            # Can't access bound port, kill the bind
+            echo "Can't access bound port, kill the bind"
             pkill -f -9 "$PAT"
           fi
         fi
+
+        echo "Monitor exiting"
       '';
     };
   };
