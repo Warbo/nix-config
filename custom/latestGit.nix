5da24e21a22cca917f825fba83ed6ba1a25ee9a4
Chris Warburton <chriswarbo@gmail.com>
2017-01-30 12:28:05 +0000

    Die if repo unavailable

--- custom/latestGit.nix
+++ custom/latestGit.nix
@@ -50,7 +50,7 @@ let
       set -e
       REV=$(git ls-remote "${url}" "${ref}") || exit 1
       # printf is an ugly way to avoid trailing newlines
-      printf "%s" $(echo "$REV         |
+      printf "%s" $(echo "$REV"        |
                     head -n1           |
                     sed -e 's/\s.*//g' ) > "$out"
     '';
