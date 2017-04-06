11758e0870fa7f7e04442879b752fe81dd51b701
Chris Warburton <chriswarbo@gmail.com>
2017-04-06 15:39:21 +0100

    Missing paren

--- custom/helpers.nix
+++ custom/helpers.nix
@@ -89,6 +89,20 @@ with {
       done
     '';
 
+    # Like fetchgit, but doesn't check against an expected hash. Useful if the
+    # commit ID is generated dynamically.
+    fetchGitHashless = args: stdenv.lib.overrideDerivation
+      # Use a dummy hash, to appease fetchgit's assertions
+      (fetchgit (args // { sha256 = hashString "sha256" args.url; }))
+
+      # Remove the hash-checking
+      (old: {
+        outputHash     = null;
+        outputHashAlgo = null;
+        outputHashMode = null;
+        sha256         = null;
+      });
+
     reverse = lst: if lst == []
                       then []
                       else reverse (tail lst) ++ [(head lst)];
