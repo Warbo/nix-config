41576950340621726978f6de9372c2066e95cdde
Chris Warburton <chriswarbo@gmail.com>
2017-04-09 11:40:24 +0100

    Make own copy of genifunctors

--- custom/haskell/genifunctors.nix
+++ custom/haskell/genifunctors.nix
@@ -3,7 +3,7 @@ self: super:
 with self;
 with rec {
   src = fetchFromGitHub {
-    owner  = "nick8325";
+    owner  = "Warbo";
     repo   = "genifunctors";
     rev    = "797482d948a00dd80f6c0236ac1d1f28fca56c68";
     sha256 = "0mfna7jsx9hkg6c4wg41bqxgphww839gx0q5amrwbl249kwn8518";
