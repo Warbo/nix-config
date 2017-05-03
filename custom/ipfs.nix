f9aaf267ba11ec30cb029d9a939b7612e1cdb609
Chris Warburton <chriswarbo@gmail.com>
2017-05-03 16:31:09 +0100

    Clean up IPFS def

--- custom/ipfs.nix
+++ custom/ipfs.nix
@@ -1,13 +1,16 @@
 self: super:
 
-with {
-  newpkgs = self.fetchFromGitHub {
+with rec {
+  newpkgs-src = self.fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "aa429e6";
-    sha256 = "1y7j59zg2zhmqqk9srh8qmi69ar2bidir4bjyhy0h0370kfvnkrg";
+    rev    = "c44be81";
+    sha256 = "1ipsvwd8dflv7k9wagw1yaqcnwfx410bfp7lrvz8cbmj7q8whlaj";
   };
+
+  newpkgs = import "${newpkgs-src}" { config = {}; };
 };
+
 {
-  inherit (import "${newpkgs}" { config = {}; }) ipfs;
+  inherit (newpkgs) ipfs;
 }
