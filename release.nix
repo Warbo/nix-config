ce5b26b1dcea116e3e78dbcc00a640a7c63771d1
Chris Warburton <chriswarbo@gmail.com>
2017-05-03 23:57:38 +0100

    Remove LTS versions
    They take too much RAM to process, for basically no gain. We care more about
    GHC versions than package sets.

--- release.nix
+++ release.nix
@@ -33,6 +33,32 @@ haskellPkgs = with rec {
                          then mapAttrs (_: keepOurs)
                          else keepOurs)
                   versions;
-}; ours;
+
+  breakages = {
+    lazysmallcheck2012 = ghc: compareVersions ghc "7.0" == -1;
+  };
+
+  # Paths to WontFix packages, e.g. those which require newer GHC features
+  broken = [
+    [ "ghc6123" "lazysmallcheck2012" ]
+  ];
+
+  noNulls = filterAttrsRecursive (_: v: v != null);
+
+  withoutBroken = noNulls (mapAttrsRecursiveCond
+    (x: !(isDerivation x))
+    (path: value: if elem path broken
+                     then null
+                     else value)
+    ours);
+
+  # There are loads of LTS versions, which take resources to process. We prefer
+  # to check compiler versions rather than particular package sets.
+  stripLTS = filterAttrs (n: _: !(hasPrefix "lts" n));
+
+  noLTS = stripLTS withoutBroken // mapAttrs (_: stripLTS) {
+                                      inherit (withoutBroken) stable unstable;
+                                    };
+}; noLTS;
 
 }; topLevel // { haskell = haskellPkgs; }
