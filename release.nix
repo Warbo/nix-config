5754aad5be0a56f4a427d3ea2c8e3d3d7b740c50
Chris Warburton <chriswarbo@gmail.com>
2017-05-07 08:25:29 +0100

    Use tinc to get haskell package dependencies in release.nix
    We care whether or not a package builds; not whether or not Nix's haskell
    package set is correct.

--- release.nix
+++ release.nix
@@ -24,43 +24,29 @@ topLevel = listToAttrs (map (name: {
 # Select our custom Haskell packages from the various sets of Haskell packages
 # provided by nixpkgs (e.g. for different compiler versions)
 haskellPkgs = with rec {
-  keepOurs = filterAttrs (name: _: elem name haskellNames);
+  keepOurs = pkgs:
+    with rec {
+      go = filterAttrs (name: _: elem name haskellNames);
+    };
+    mapAttrs (_: go) pkgs // mapAttrs (_: keepOurs) (stableUnstableFrom pkgs);
 
   versions = { inherit haskellPackages profiledHaskellPackages; } //
              haskell.packages;
 
-  ours = mapAttrs (n: if n == "stable" || n == "unstable"
-                         then mapAttrs (_: keepOurs)
-                         else keepOurs)
-                  versions;
-
   # Paths to WontFix packages, e.g. those which require newer GHC features
   broken = with rec {
-    # Remove GHC versions prior to some release
-    post74 = name: [
-      [ "ghc704" name ]
-      [ "ghc722" name ]
-    ];
-    post76 = name: post74 name ++ [
-      [ "ghc742" name ]
-    ];
-    post78 = name: post76 name ++ [
-      [ "ghc763" name ]
-    ];
-    post710 = name: base47 name ++ [
-      [ "ghc783" name ]
-      [ "ghc784" name ]
-    ];
+    # Flags GHC versions older than some release
+    post74  = name:                [ [ "ghc704" name ] [ "ghc722" name ] ];
+    post76  = name: post74 name ++ [ [ "ghc742" name ]                   ];
+    post78  = name: post76 name ++ [ [ "ghc763" name ]                   ];
+    post710 = name: base47 name ++ [ [ "ghc783" name ] [ "ghc784" name ] ];
 
-    # base >= 4.7 requires GHC >= 7.8
-    base47 = post78;
-
-    # base >= 4.8 requires GHC >= 7.10
-    base48 = post710;
-
-    # template-haskell >= 2.9 requires GHC >= 7.6.3
-    th29 = post76;
+    # Flags GHC versions without some feature
+    base47 = post78;   # base >= 4.7 requires GHC >= 7.8
+    base48 = post710;  # base >= 4.8 requires GHC >= 7.10
+    th29   = post76;   # template-haskell >= 2.9 requires GHC >= 7.6.3
   };
+  # Packages which depend on recent GHC versions
   base48  "ifcxt"              ++
   base48  "ghc-dup"            ++
   base48  "haskell-example"    ++
@@ -70,26 +56,41 @@ haskellPkgs = with rec {
   post78  "ghc-simple"         ++
   post74  "tip-lib";
 
-  noNulls = filterAttrsRecursive (_: v: v != null);
-
-  # If a package is known to break, check that it does (otherwise we might be
-  # ignoring perfectly valid targets!). Note that this will only check that the
-  # package itself is broken; not whether or not it has working dependencies.
-  checkBroken = noNulls (mapAttrsRecursiveCond
+  # Check that known breakages are, in fact, broken. Leave others as-is
+  checkBroken = mapAttrsRecursiveCond
     (x: !(isDerivation x))
     (path: value: if elem path broken
                      then isBroken value
-                     else value)
-    ours);
+                     else value);
+
+  stableUnstableFrom = sets:
+    fold (name: result: if hasAttr name sets
+                           then result // {
+                                  "${name}" = sets."${name}";
+                                }
+                           else result)
+         {}
+         [ "stable" "unstable" ];
 
   # There are loads of LTS versions, which take resources to process. We prefer
   # to check compiler versions rather than particular package sets.
-  # GHC 6.12.3 is marked as broken, but for some reason is still included...
-  stripLTS = filterAttrs (n: _: !(hasPrefix "lts" n) && n != "ghc6123");
+  # GHC 6.12.3 is marked as broken, so refuses to evaluate at all.
+  stripLTS = sets:
+    filterAttrs (n: _: !(hasPrefix "lts" n) && n != "ghc6123") sets //
+    mapAttrs (_: stripLTS) (stableUnstableFrom sets);
 
-  noLTS = stripLTS checkBroken // mapAttrs (_: stripLTS) {
-                                      inherit (checkBroken) stable unstable;
-                                    };
-}; noLTS;
+  # Use tinc for dependencies. This gives us per-package dependencies, chosen
+  # from hackage by cabal, written in the form of Nix expressions.
+  tincified =
+    mapAttrs (version: pkgs:
+               if elem version [ "stable" "unstable" ]
+                  then tincified pkgs
+                  else mapAttrs (name: pkg: tincify {
+                                              inherit (pkg) src;
+                                              name = "${name}-${version}";
+                                              haskellPackages = pkgs;
+                                            })
+                                pkgs);
+}; keepOurs (checkBroken (tincified (stripLTS versions)));
 
 }; topLevel // { haskell = haskellPkgs; }
