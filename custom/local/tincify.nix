a9884ed62ee4534613192c2a5847cca9f51d2206
Chris Warburton <chriswarbo@gmail.com>
2017-05-07 08:24:30 +0100

    "tincify" to get the dependencies of a Haskell package using tinc

---
+++ custom/local/tincify.nix
@@ -0,0 +1,40 @@
+{ cabal2nix, hackageDb, haskellPackages, haskellTinc, runCommand, sanitiseName,
+  withNix }:
+
+with builtins;
+with rec {
+  defHPkg = haskellPackages;
+
+  tincify = { src, name ? "pkg", haskellPackages ? defHPkg,
+              pkgs ? import <nixpkgs> {} }:
+    with rec {
+      defs = runCommand "tinc-of-${name}"
+               (withNix {
+                 inherit src hackageDb;
+                 buildInputs = [
+                   haskellTinc
+                   (haskellPackages.ghcWithPackages (h: [ h.cabal-install ]))
+                   cabal2nix
+                 ];
+                 TINC_USE_NIX = "yes";
+               })
+               ''
+                 cp -r "$hackageDb" ./home
+                 chmod +w -R ./home
+                 export HOME="$PWD/home"
+
+                 cp -r "$src" ./src
+                 chmod +w -R ./src
+
+                 pushd ./src; tinc; popd
+
+                 cp -r ./src "$out"
+               '';
+
+      deps = import "${defs}/tinc.nix" {
+        nixpkgs = pkgs // { inherit haskellPackages; };
+      };
+    };
+    deps.resolver.callPackage "${defs}/package.nix" {};
+};
+tincify
