9a6b4c61a516a1b5f3b72201bbb67c00be510e1e
Chris Warburton <chriswarbo@gmail.com>
2017-05-09 16:11:16 +0100

    Override NIX_PATH when running tinc
    Internally, the `tinc` command calls:
    
      nix-shell -p 'haskellPackages.ghcWithPackages (p: [ p.cabal-install ])'
                --pure --run 'cabal ....'
    
    Using the haskellPackages global in this way completely bypasses whichever
    haskell package set was given to tincify. To remedy this, we need to serialise
    the given package set into a file, and read it back in tinc's nix-shell call.
    
    Since we know that only cabal-install is needed, we build this derivation
    up-front. We then construct a directory containing a .nix file which only
    provides a haskellPackages.ghcWithPackages attribute, which is hard-coded to
    return our pre-built derivation. We override NIX_PATH to use this directory as
    <nixpkgs>, so that nix-shell will get the right environment.

--- custom/local/tincify.nix
+++ custom/local/tincify.nix
@@ -1,4 +1,5 @@
-{ cabal2nix, hackageDb, haskellPackages, haskellTinc, runCommand, withNix }:
+{ buildEnv, cabal2nix, hackageDb, haskellPackages, haskellTinc, runCommand,
+  withNix, writeScript }:
 
 with builtins;
 with rec {
@@ -7,6 +8,50 @@ with rec {
   tincify = { src, name ? "pkg", haskellPackages ? defHPkg,
               pkgs ? import <nixpkgs> {} }:
     with rec {
+      # By default, tinc runs Cabal in a Nix shell with the following available:
+      #
+      #   haskellPackages.ghcWithPackages (p: [ p.cabal-install ])'
+      #
+      # This can be fiddled a bit using TINC_NIX_RESOLVER, but overall we're
+      # stuck using a Haskell package set that's available globally with a
+      # particular attribute path. We don't want that; we want to use the
+      # Haskell package set we've been given (haskellPackages), which might not
+      # be available globally.
+      #
+      # To make tinc use this version, we rely on the fact it's only using
+      # cabal-install, as above. We build that derivation, using our Haskell
+      # package set, write it to disk, then set NIX_PATH such that tinc's
+      # nix-shell invocation uses the derivation we built. Phew!
+      toUse = buildEnv {
+        name  = "tinc-env";
+        paths = [ (haskellPackages.ghcWithPackages (p: [ p.cabal-install ])) ];
+      };
+
+      env = runCommand "tinc-env"
+        {
+          expr = writeScript "force-tinc-env.nix" ''
+            _:
+              import <real> {} // {
+              haskellPackages = {
+                ghcWithPackages = _:
+                  ${toUse};
+              };
+            }
+          '';
+        }
+        ''
+          mkdir -p "$out/pkgs/build-support/fetchurl"
+          cp "$expr" "$out/default.nix"
+          cp "${<nixpkgs/pkgs/build-support/fetchurl/mirrors.nix>}" \
+             "$out/pkgs/build-support/fetchurl/mirrors.nix"
+        '';
+
+      NIX_PATH = concatStringsSep ":" [
+        "nixpkgs=${env}"
+        "real=${(head (filter (p: p.prefix == "nixpkgs")
+                              nixPath)).path}"
+      ];
+
       defs = runCommand "tinc-of-${name}"
                (withNix {
                  inherit src hackageDb;
@@ -19,7 +64,8 @@ with rec {
                    cabal2nix
                  ];
                  TINC_USE_NIX = "yes";
-               })
+               } // {
+                 inherit NIX_PATH; })
                ''
                  cp -r "$hackageDb" ./home
                  chmod +w -R ./home
