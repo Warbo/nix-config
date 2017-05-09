2f75a41a98ee825390e66c38316ea17de72738b2
Chris Warburton <chriswarbo@gmail.com>
2017-05-09 09:27:03 +0100

    PanPipe needs GHC 7.10.*

--- custom/local/panpipe.nix
+++ custom/local/panpipe.nix
@@ -1,5 +1,8 @@
-{ haskellPackages, tincify }:
+{ haskell, tincify }:
 
+with {
+  haskellPackages = haskell.ghc7103.packages;  # For base 4.8.*
+};
 tincify {
   inherit haskellPackages;
   inherit (haskellPackages.panpipe) name src;
