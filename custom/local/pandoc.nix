ddd6983f93d755875f6d1b8008ebf048aeb06ab1
Chris Warburton <chriswarbo@gmail.com>
2017-05-17 09:50:33 +0100

    Disable pandoc tests

--- custom/local/pandoc.nix
+++ custom/local/pandoc.nix
@@ -1,6 +1,16 @@
-{ haskellPackages, runCabal2nix, tincify }:
+{ haskell, haskellPackages, runCabal2nix, tincify }:
 
-# We use 1.17.2 for compatibility with panpipe 0.2 and panhandle 0.2
-tincify (haskellPackages.callPackage (runCabal2nix {
-          url = "cabal://pandoc-1.17.2";
-        }) {})
+with rec {
+  # We use 1.17.2 for compatibility with panpipe 0.2 and panhandle 0.2
+  def = runCabal2nix { url = "cabal://pandoc-1.17.2"; };
+
+  # Turn definition into a package with haskellPackages
+  pkg = haskellPackages.callPackage def {};
+
+  # Use tinc for dependencies rather than those hardcoded in haskellPackages
+  withDeps = tincify pkg;
+
+  # Writer tests fail since they can't access the disk, so we disable them all
+  result = haskell.lib.dontCheck withDeps;
+};
+result
