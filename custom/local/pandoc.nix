f60ac7ea742f5d69f259370f7fae66db0938c5d8
Chris Warburton <chriswarbo@gmail.com>
2017-05-17 08:42:37 +0100

    Use pandoc 1.17.2

--- custom/local/pandoc.nix
+++ custom/local/pandoc.nix
@@ -1,4 +1,6 @@
-{ haskellPackages, latestCabal, tincify }:
+{ haskellPackages, runCabal2nix, tincify }:
 
-#tincify (latestCabal "pandoc")
-haskellPackages.pandoc
+# We use 1.17.2 for compatibility with panpipe 0.2 and panhandle 0.2
+tincify (haskellPackages.callPackage (runCabal2nix {
+          url = "cabal://pandoc-1.17.2";
+        }) {})
