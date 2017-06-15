9a8eab7f7470fc7c1a5c235b14517dc3d87be1fb
Chris Warburton <chriswarbo@gmail.com>
2017-06-15 18:29:16 +0100

    Added shouldFail
    Useful for tests, we can say 'shouldFail myDrv', and get back a derivation which
    succeeds (outputting a message to "$out") when myDrv's 'builder' fails, and
    fails when myDrv's 'builder' succeeds.
    
    This can be especially useful if we're implementing some workaround. For
    example, if 'somePkg' doesn't work with the latest GHC, we might override it to
    use a previous GHC. In such cases, we might also add 'shouldFail originalPkg' as
    a dependency, which will trigger a build failure when our workaround is no
    longer needed.

---
+++ custom/local/shouldFail.nix
@@ -0,0 +1,16 @@
+{ bash, stdenv, writeScript }:
+
+toFail: stdenv.lib.overrideDerivation toFail (old: {
+  builder = writeScript "toFail-${old.name}" ''
+    #!${bash}/bin/bash
+    echo "Ensuring that ${old.name} fails to build" 1>&2
+    if "${old.builder}" ${builtins.concatStringsSep " " old.args}
+    then
+      echo "Error: ${old.name} succeeded but should have failed" 1>&2
+      exit 1
+    fi
+
+    echo "shouldFail: ${old.name} failed to build, as we expected" 1>&2
+    echo "Failed as expected" > "$out"
+  '';
+})
