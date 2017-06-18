86f9ef3c5287dff0380eb079ec7e11ccd71e7e98
Chris Warburton <chriswarbo@gmail.com>
2017-06-18 23:22:47 +0100

    Partial redundancy check for ifcxt override

--- custom/haskell/ifcxt.nix
+++ custom/haskell/ifcxt.nix
@@ -1,6 +1,29 @@
 self: super: with self;
 
-haskellGit {
-  url = "${repoSource}/ifcxt.git";
-  ref = "constraints";
-}
+with rec {
+  new = haskellGit {
+    url = "${repoSource}/ifcxt.git";
+    ref = "constraints";
+  };
+
+  # Forces nix-eval to use non-overriden version of ifcxt
+  NIX_EVAL_HASKELL_PKGS = writeScript "ghc7.10-for-nix-eval.nix" ''
+    with import <nixpkgs> {};
+    haskell.packages.ghc7103 // {
+      inherit (origPkgs.haskell.packages.ghc7103) ifcxt;
+    }
+  '';
+
+  broken = runCommand "runtime-arbitrary-tests"
+    (withNix {
+      inherit NIX_EVAL_HASKELL_PKGS;
+      buildInputs = [ haskellPackages.runtime-arbitrary-tests ];
+    })
+    ''
+      runtime-arbitrary-tests && echo "Pass" > "$out"
+    '';
+};
+withArgsOf new (args:
+  haskell.lib.addExtraLibrary (new args) {
+    #overrideStillRequired = shouldFail broken;
+  })
