cbdc6666a763ed11704d9aa92196ffc4f54d7efc
Chris Warburton <chriswarbo@gmail.com>
2017-06-19 01:14:44 +0100

    Disable ASV checks

--- custom/local/asv.nix
+++ custom/local/asv.nix
@@ -1,22 +1,28 @@
-{ fetchFromGitHub, git, pythonPackages }:
+{ fetchFromGitHub, git, pythonPackages, shouldFail }:
 
-pythonPackages.buildPythonPackage {
-  name = "airspeed-velocity";
-  src  = fetchFromGitHub {
-    owner  = "spacetelescope";
-    repo   = "asv";
-    rev    = "953c960";
-    sha256 = "10fissqb3fzqs94c9b0rzd9gk1kxccn13bfh22rjih4z9jdfh113";
+with {
+  plain = pythonPackages.buildPythonPackage {
+    name = "airspeed-velocity";
+    src  = fetchFromGitHub {
+      owner  = "spacetelescope";
+      repo   = "asv";
+      rev    = "953c960";
+      sha256 = "10fissqb3fzqs94c9b0rzd9gk1kxccn13bfh22rjih4z9jdfh113";
+    };
+
+    # For tests
+    buildInputs = [
+      git
+      pythonPackages.virtualenv
+      pythonPackages.pip
+      pythonPackages.wheel
+    ];
+
+    # For resulting scripts
+    propagatedBuildInputs = with pythonPackages; [ six ];
   };
-
-  # For tests
-  buildInputs = [
-    git
-    pythonPackages.virtualenv
-    pythonPackages.pip
-    pythonPackages.wheel
-  ];
-
-  # For resulting scripts
-  propagatedBuildInputs = with pythonPackages; [ six ];
-}
+};
+plain.override (old: {
+  stillNeedToDisableTests = shouldFail plain;
+  doCheck = false;
+})
