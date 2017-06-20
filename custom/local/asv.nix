5b4a9374f54c154a33f8164072338946ed8d4be1
Chris Warburton <chriswarbo@gmail.com>
2017-06-20 01:24:31 +0100

    Patch asv to chmod the template dirs to be writable
    Since they get stored in the read-only Nix store, when we copy them out they
    don't have write permission enabled, and hence we can't edit the contents (to
    fill in the data)

--- custom/local/asv.nix
+++ custom/local/asv.nix
@@ -10,6 +10,14 @@ with {
       sha256 = "10fissqb3fzqs94c9b0rzd9gk1kxccn13bfh22rjih4z9jdfh113";
     };
 
+    postPatch = ''
+      substituteInPlace asv/commands/publish.py \
+        --replace 'shutil.copytree(template_dir, conf.html_dir)' \
+                  'shutil.copytree(template_dir, conf.html_dir)
+              os.chmod(conf.html_dir, 0755)
+              [[os.chmod(pre+"/"+x, 0755) for x in (fs+ds)] for (pre, ds, fs) in os.walk(conf.html_dir)]'
+    '';
+
     # For tests
     buildInputs = [
       git
