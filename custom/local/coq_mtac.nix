4f1ce04d0f01ddc246c5763929a387b114151d66
Chris Warburton <chriswarbo@gmail.com>
2017-06-28 15:25:29 +0100

    Use Coq from nixpkgs 16.09

--- custom/local/coq_mtac.nix
+++ custom/local/coq_mtac.nix
@@ -1,6 +1,6 @@
-{ stdenv, coq, latestGit }:
+{ nixpkgs1609, stdenv, latestGit }:
 
-stdenv.lib.overrideDerivation coq (oldAttrs : {
+stdenv.lib.overrideDerivation nixpkgs1609.coq (oldAttrs : {
   name  = "coq-mtac";
   src   = latestGit {
     url = https://github.com/beta-ziliani/coq.git;
