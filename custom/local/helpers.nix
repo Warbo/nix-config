07041ec61b8c75d075480ea6df388513bd5f7d26
Chris Warburton <chriswarbo@gmail.com>
2017-08-17 00:29:27 +0100

    Split up "helpers" into individual packages

---
+++ custom/local/helpers.nix
@@ -0,0 +1,5 @@
+# TODO: Maybe obsolete?
+
+{ forceBuilds, isPath, repoSource, attrsToDirs, dirContaining, dirsToAttrs,
+mergeDirs, fetchGitHashless, reverse, sanitiseName, suffMatch, repo2npm,
+withDeps }@args: args
