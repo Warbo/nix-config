9b126a5c43401f598db797f9ec312639f885757f
Chris Warburton <chriswarbo@gmail.com>
2017-08-18 18:16:20 +0100

    Add test runner (prevents typing the same thing over and over!)

---
+++ runTests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+BASE=$(dirname "$(readlink -f "$0")")
+
+export F="$BASE/test.nix"
+
+[[ -e "$F" ]] || {
+    echo "Couldn't find test.nix file '$F', aborting" 1>&2
+    exit 1
+}
+
+if nix-build --show-trace --no-out-link -E 'import (builtins.getEnv "F")'
+then
+    echo "ok - nix-config tests"
+else
+    echo "not ok - nix-config tests"
+    exit 1
+fi
+
+exit
