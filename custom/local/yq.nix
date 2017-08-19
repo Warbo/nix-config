d08dc0012bcd133cd9e1a81e7b8cfdc38b04b7ec
Chris Warburton <chriswarbo@gmail.com>
2017-08-19 02:19:04 +0100

    Force yq to use particular nixpkgs version
    Needed to satisfy setuptool >= 30

--- custom/local/yq.nix
+++ custom/local/yq.nix
@@ -1,13 +1,18 @@
-{ fetchurl, jq, pythonPackages }:
+{ fetchurl, getNixpkgs, jq }:
 
-pythonPackages.buildPythonPackage {
+# Requires setuptools >= 30, so we use a fixed nixpkgs version
+with getNixpkgs {
+  rev    = "4b0afc1";
+  sha256 = "1yz3giqiza9xcblaiz1ic8a7fyhll0ngxy9nm04phyxd0nrdfn6b";
+};
+pkgs.pythonPackages.buildPythonPackage {
   name = "yq";
   src  = fetchurl {
     url    = https://pypi.python.org/packages/14/1b/5efddd608b7df9849be50aca4a0d8602b75fb2929223a44e241d7290d6ea/yq-2.1.1.tar.gz;
     sha256 = "0g7rbmfn7k4rz77iqg29kp24fjpl678gy1g17hx435sdwjns00pd";
   };
-  propagatedBuildInputs = with pythonPackages; [
+  propagatedBuildInputs = [
     jq
-    pyyaml
+    pkgs.pythonPackages.pyyaml
   ];
 }
