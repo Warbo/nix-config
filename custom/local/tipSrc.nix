168469af90d86a6e217d215e58c6727e6ecf29d1
Chris Warburton <chriswarbo@gmail.com>
2017-09-12 15:45:38 +0100

    Fix hash of tipSrc

--- custom/local/tipSrc.nix
+++ custom/local/tipSrc.nix
@@ -4,5 +4,5 @@ fetchFromGitHub {
   owner  = "Warbo";
   repo   = "tools";
   rev    = "2b8ed14";  # Custom fixes
-  sha256 = "1ibf0gd2wig58a20r3jaj3yiqxi981f75fcsss5czwnk9p9yv3vb";
+  sha256 = "1nmja63agidjfspppgsrd6fv7fzc3vadw528cwj9vqajs9sgnla5";
 }
