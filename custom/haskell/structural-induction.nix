915742b418676146a9c2ea675424245712685461
Chris Warburton <chriswarbo@gmail.com>
2017-10-03 17:20:20 +0100

    Simplify structural-induction

--- custom/haskell/structural-induction.nix
+++ custom/haskell/structural-induction.nix
@@ -1,19 +1,3 @@
 self: super:
 
-with self;
-with {
-  repoHack = runCommand "repo-hack" {} ''
-    mkdir -p "$out"
-    cp -r "${stableRepo}" "$out/nixpkgs"
-  '';
-};
-import (runCommand "get" {
-    buildInputs = [ cabal2nix cabal-install nix-prefetch-scripts nix ];
-    NIX_REMOTE  = "daemon";
-    NIX_PATH    = builtins.getEnv "NIX_PATH" + ":${repoHack}";
-  }
-  ''
-    export HOME="$PWD"
-    cabal update
-    cabal2nix cabal://structural-induction-0.3 > "$out"
-  '')
+self.runCabal2nix { url = "cabal://structural-induction-0.3"; }
