b8fe4fe5e15a84d6df9e519888c01bc19493b4fc
Chris Warburton <chriswarbo@gmail.com>
2018-01-10 16:40:15 +0000

    Bump warbo-utilities

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -4,7 +4,7 @@ withLatestGit {
   url      = "${repoSource}/warbo-utilities.git";
   srcToPkg = src: import "${src}" { nixPkgs = self; };
   stable   = {
-    rev    = "c7b6e4b";
-    sha256 = "1nw0i5nsdhx3zpi2djqsvb9aixiyinn01sxq32047n4p44zv5s0w";
+    rev    = "457be3b";
+    sha256 = "19pyzhy76v7z8y0i51hhpdnz4667x4kb0yvys87sps0yd3359h5g";
   };
 }
