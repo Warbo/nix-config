2b9d29825ae97cb7834873a5fdae0f84cb44b0d0
Chris Warburton <chriswarbo@gmail.com>
2018-01-11 00:27:05 +0000

    Bump warbo-utilities to avoid hard-coded /home/chris path

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -4,7 +4,7 @@ withLatestGit {
   url      = "${repoSource}/warbo-utilities.git";
   srcToPkg = src: import "${src}" { nixPkgs = self; };
   stable   = {
-    rev    = "457be3b";
-    sha256 = "19pyzhy76v7z8y0i51hhpdnz4667x4kb0yvys87sps0yd3359h5g";
+    rev    = "90ec278";
+    sha256 = "0nvgp4ajrlxy6x1pw2kkqvimj4qpykww6b9p126gycmp7zaylv2w";
   };
 }
