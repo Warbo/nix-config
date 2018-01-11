476915f0a9002d587369c033cc4cd7d9c201a9bd
Chris Warburton <chriswarbo@gmail.com>
2018-01-11 16:45:40 +0000

    Force all overrides to use stable nixpkgs version instead of <nixpkgs>
    Our custom packages and overrides are prone to breaking, which is annoying since
    Nix is meant to aid isolation and reproducibility.
    
    One of the main reasons is that we're extending the <nixpkgs> set, which may
    vary across systems. To remove this ambiguity, we now use a fixed, known nixpkgs
    set (currently nixpkgs1603) as the basis for all of our packages/overrides.
    
    We actually provide packages/overrides for various nixpkgs sets (16.03, 16.09,
    etc.) including the system's <nixpkgs> (which we call "unstable"). These are
    all added to an attribute called 'customised'.
    
    For each custom thing 'foo' there will be a 'customised.nixpkgs1603.foo', a
    'customised.nixpkgs1609.foo', etc. as well as a
    'customised.stable.foo' ('stable' is currently a synonym for 'nixpkgs1603') and
    'customised.unstable.foo' (which is based on <nixpkgs>).
    
    The top-level 'foo' will be taken from 'customised.stable', rather than
    'customised.unstable' (which would be equivalent to the old, unreliable
    behaviour).

--- stable.nix
+++ stable.nix
@@ -1,3 +1,36 @@
-import ./other.nix // {
-  packageOverrides = import ./custom.nix true;
+with rec {
+  # Various immutable versions of nixpkgs
+  nixpkgs = import ./nixpkgs.nix;
+
+  inherit (nixpkgs.nixpkgs1603) lib;
+
+  # Just the nixpkgs repos (ignores instantiated package sets, functions, etc.)
+  repos = lib.filterAttrs (name: _: lib.hasPrefix "repo" name ||
+                                    name == "stableRepo")
+                          nixpkgs;
+
+  # Generates overrides for a package set. Also takes a bool to pick un/stable.
+  custom = import ./custom.nix;
+
+  # Load each "repoFOO", applying our overrides and renaming to "nixpkgsFOO"
+  customised = lib.mapAttrs'
+                 (name: repo: {
+                   name  = if name == "stableRepo"
+                              then "stable"
+                              else "nixpkgs" + lib.removePrefix "repo" name;
+                   value = import repo {
+                     config = other // {
+                       packageOverrides = pkgs:
+                         nixpkgs // custom true (nixpkgs // pkgs);
+                     };
+                   };
+                 })
+                 repos;
+
+  other = import ./other.nix;
+};
+other // {
+  packageOverrides = pkgs: nixpkgs // customised.stable // {
+    customised = customised // { unstable = custom false pkgs; };
+  };
 }
