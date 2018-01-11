37db5f2acd9111cbf051701e65cf54be8ecde43e
Chris Warburton <chriswarbo@gmail.com>
2018-01-11 15:59:20 +0000

    Fetch nixpkgs versions without external help

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,15 +1,19 @@
-# Stable nixpkgs
-self: super:
+# Stable nixpkgs repos
 
 with builtins;
-with super.lib;
 with rec {
-  nixpkgsRepo = { rev, sha256 }:
-    super.fetchFromGitHub {
-      inherit rev sha256;
-      owner = "NixOS";
-      repo  = "nixpkgs";
-    };
+  # Only use the system's <nixpkgs> for essentials, e.g. fixed-output git
+  # fetchers and pure utility functions.
+  unstablePkgs = import <nixpkgs> { config = {}; };
+
+  inherit (unstablePkgs)     fetchFromGitHub;
+  inherit (unstablePkgs.lib) mapAttrs mapAttrs';
+
+  nixpkgsRepo = { rev, sha256 }: fetchFromGitHub {
+    inherit rev sha256;
+    owner = "NixOS";
+    repo  = "nixpkgs";
+  };
 
   # Explicitly pass an empty config, to avoid loading ~/.nixpkgs/config.nix
   # and causing an infinite loop
@@ -17,15 +21,15 @@ with rec {
 
   repos = mapAttrs (_: nixpkgsRepo) {
     repo1603 = {
-      rev    = "16.03";
+      rev    = "d231868";
       sha256 = "0m2b5ignccc5i5cyydhgcgbyl8bqip4dz32gw0c6761pd4kgw56v";
     };
     repo1609 = {
-      rev    = "16.09";
+      rev    = "f22817d";
       sha256 = "1cx5cfsp4iiwq8921c15chn1mhjgzydvhdcmrvjmqzinxyz71bzh";
     };
     repo1703 = {
-      rev    = "17.03";
+      rev    = "1849e69";
       sha256 = "1fw9ryrz1qzbaxnjqqf91yxk1pb9hgci0z0pzw53f675almmv9q2";
     };
     repo1709 = {
