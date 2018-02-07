eb32afd93ba00cd14d5cf58615d0a5e6a75eb93f
Chris Warburton <chriswarbo@gmail.com>
2018-02-07 19:12:13 +0000

    Don't drag old nixpkgs around for yq

--- custom/local/yq.nix
+++ custom/local/yq.nix
@@ -1,11 +1,6 @@
-{ fetchurl, getNixpkgs, jq }:
+{ fetchurl, jq, pythonPackages }:
 
-# Requires setuptools >= 30, so we use a fixed nixpkgs version
-with getNixpkgs {
-  rev    = "4b0afc1";
-  sha256 = "1yz3giqiza9xcblaiz1ic8a7fyhll0ngxy9nm04phyxd0nrdfn6b";
-};
-pkgs.pythonPackages.buildPythonPackage {
+pythonPackages.buildPythonPackage {
   name = "yq";
   src  = fetchurl {
     url    = https://pypi.python.org/packages/14/1b/5efddd608b7df9849be50aca4a0d8602b75fb2929223a44e241d7290d6ea/yq-2.1.1.tar.gz;
@@ -13,6 +8,6 @@ pkgs.pythonPackages.buildPythonPackage {
   };
   propagatedBuildInputs = [
     jq
-    pkgs.pythonPackages.pyyaml
+    pythonPackages.pyyaml
   ];
 }
