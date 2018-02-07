86b11d80997b7b8b165fbc6fb46f56bb71feb146
Chris Warburton <chriswarbo@gmail.com>
2018-02-07 17:59:17 +0000

    Stop using old versions of things to build skulpture

--- custom/local/skulpture.nix
+++ custom/local/skulpture.nix
@@ -1,6 +1,5 @@
-{ cmake, fetchurl, findutils, nixpkgs1603 }:
+{ cmake, fail, fetchurl, findutils, kdelibs4, stdenv, writeScript }:
 
-with { inherit (nixpkgs1603) kde4 stdenv; };
 stdenv.mkDerivation {
   name    = "skulpture";
   version = "0.2.4";
@@ -9,7 +8,15 @@ stdenv.mkDerivation {
     sha256 = "1s27xqd32ck09r1nnjp1pyxwi0js7a7rg2ppkvq2mk78nfcl6sk0";
   };
 
-  buildInputs = [ cmake kde4.kdelibs findutils ];
+  toMove = writeScript "toMove.tsv" ''
+    skulpture.themerc	share/kde4/apps/kstyle/themes
+    skulpture.desktop	share/kde4/apps/kwin
+    skulpture.png	share/kde4/apps/skulpture/pics
+    skulptureui.rc	share/kde4/apps/skulpture
+    libskulpture.so	lib/qt4/plugins/styles
+  '';
+
+  buildInputs = [ cmake fail kdelibs4 findutils ];
 
   installPhase = ''
     cd ..
@@ -17,7 +24,7 @@ stdenv.mkDerivation {
     mkdir -p "$out/share/doc"
     for DOC in README AUTHORS COPYING NEWS NOTES BUGS
     do
-        cp -v "$DOC" "$out/share/doc/"
+      cp -v "$DOC" "$out/share/doc/"
     done
 
     mkdir -p "$out/share/kde4/apps/"
@@ -25,8 +32,8 @@ stdenv.mkDerivation {
 
     while read -r PAIR
     do
-      F=$(echo "$PAIR" | cut -f 1)
-      D=$(echo "$PAIR" | cut -f 2)
+      F=$(echo "$PAIR" | cut -f1)
+      D=$(echo "$PAIR" | cut -f2)
       mkdir -p "$out/$D"
       FOUND=0
       while read -r GOT
@@ -34,13 +41,7 @@ stdenv.mkDerivation {
         cp -rv "$GOT" "$out/$D/"
         FOUND=1
       done < <(find . -name "$F")
-      [[ "$FOUND" -eq 1 ]] || exit 1
-    done << EOF
-    skulpture.themerc	share/kde4/apps/kstyle/themes
-    skulpture.desktop	share/kde4/apps/kwin
-    skulpture.png	share/kde4/apps/skulpture/pics
-    skulptureui.rc	share/kde4/apps/skulpture
-    libskulpture.so	lib/qt4/plugins/styles
-    EOF
+      [[ "$FOUND" -eq 1 ]] || fail "Didn't find '$F', aborting"
+    done < "$toMove"
   '';
 }
