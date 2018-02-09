71c6087324a16e376449759539224cd041cef2b9
Chris Warburton <chriswarbo@gmail.com>
2018-02-09 17:35:38 +0000

    Pass self to warbo-utilities, for consistency

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -1,10 +1,8 @@
-{ repoSource, self, stable, withLatestGit }:
+{ repoSource, self, withLatestGit }:
 
 withLatestGit {
   url      = "${repoSource}/warbo-utilities.git";
-  srcToPkg = src: import "${src}" (if stable
-                                      then {}
-                                      else { nixPkgs = self; });
+  srcToPkg = src: import "${src}" { nixPkgs = self; };
   stable   = {
     rev    = "004f327";
     sha256 = "1dhyhr6vph84x35wckibnfmlmbz6fwb2lb3m9hs367al0lzgxbpz";
