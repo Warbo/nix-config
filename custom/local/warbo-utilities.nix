2ef42e61622648904d76f792aad0adcdcf5691b2
Chris Warburton <chriswarbo@gmail.com>
2018-02-09 17:35:25 +0000

    Bump warbo-utilities

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -6,7 +6,7 @@ withLatestGit {
                                       then {}
                                       else { nixPkgs = self; });
   stable   = {
-    rev    = "bf69378";
-    sha256 = "1a1ss17cvc94xg83vjgwbqc6aphx2xy3hapn2j1lk464l2iwd1bn";
+    rev    = "004f327";
+    sha256 = "1dhyhr6vph84x35wckibnfmlmbz6fwb2lb3m9hs367al0lzgxbpz";
   };
 }
