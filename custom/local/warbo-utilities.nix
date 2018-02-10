ba9970f8197354daa40015787188187bba22bf2b
Chris Warburton <chriswarbo@gmail.com>
2018-02-10 19:21:48 +0000

    Take ipfs from 1709 for now, since 1609 is marked as broken

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -1,8 +1,18 @@
-{ repoSource, self, withLatestGit }:
+{ nixpkgs1709, repo, repoSource, runCommand, self, withLatestGit }:
 
+with rec {
+  version = import (runCommand "nixpkgs-version.nix" { inherit repo; } ''
+    V=$(cat "$repo/.version")
+    echo "\"$V\"" > "$out"
+  '');
+
+  nixPkgs = self // (if version == "17.09"
+                        then {}
+                        else { inherit (nixpkgs1709) ipfs; });
+};
 withLatestGit {
   url      = "${repoSource}/warbo-utilities.git";
-  srcToPkg = src: import "${src}" { nixPkgs = self; };
+  srcToPkg = src: import "${src}" { inherit nixPkgs; };
   stable   = {
     rev    = "004f327";
     sha256 = "1dhyhr6vph84x35wckibnfmlmbz6fwb2lb3m9hs367al0lzgxbpz";
