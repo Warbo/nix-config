7b0d6b22fd56c90c21cd0f281d2ed2141eae027b
Chris Warburton <chriswarbo@gmail.com>
2018-02-13 11:58:36 +0000

    Screw it, take Basket from 16.09

--- custom/local/basket.nix
+++ custom/local/basket.nix
@@ -1,13 +1,3 @@
-{ kde4, repo1609, self }:
+{ kde4, nixpkgs1609 }:
 
-with rec {
-  kdelibs = self.kdelibs4 or kde4.kdelibs;
-
-  oldPkg  = self.callPackage "${repo1609}/pkgs/applications/office/basket" {
-    inherit kdelibs;
-    inherit (kde4) kdepimlibs;
-  };
-};
-if kde4 ? basket
-   then kde4.basket
-   else oldPkg
+kde4.basket or nixpkgs1609.kde4.basket
