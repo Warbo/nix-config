a10bb443daf2e93905d52d635bac35039b8624fc
Chris Warburton <chriswarbo@gmail.com>
2018-02-27 20:27:47 +0000

    Use lucid emacs instead of GTK

--- custom/local/emacs.nix
+++ custom/local/emacs.nix
@@ -4,7 +4,10 @@ with rec {
   version = if emacs25 == null
                then super.emacs
                else emacs25;
+
+  # GTK crashes if X restarts, plus GTK3 is horrible and it's slow
+  lucid = version.override { withGTK2 = false; withGTK3 = false; };
 };
-(emacsPackagesNgGen version).emacsWithPackages (epkgs:
+(emacsPackagesNgGen lucid).emacsWithPackages (epkgs:
   (with epkgs.elpaPackages;  []) ++
   (with epkgs.melpaPackages; []))
