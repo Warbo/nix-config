2e23e6851b613f2c17caf904532e4234e6106acb
Chris Warburton <chriswarbo@gmail.com>
2018-03-26 12:13:13 +0100

    Revert unstable conkeror

--- custom/local/conkeror.nix
+++ custom/local/conkeror.nix
@@ -1,23 +1,3 @@
-{ firefox, latestGit, makeWrapper, stdenv, unzip }:
+{ nixpkgs1609 }:
 
-stdenv.mkDerivation rec {
-  inherit firefox;
-  pkgname      = "conkeror";
-  version      = "git";
-  name         = "${pkgname}-${version}";
-  src          = latestGit {
-    url    = https://github.com/retroj/conkeror.git;
-    stable = {
-      rev    = "97115c2";
-      sha256 = "1ikjzfvp7wm9f0644d0dxdlfbm2xf46fxm7njxwnb9s086c9rrvw";
-    };
-  };
-  buildInputs  = [ unzip makeWrapper ];
-  installPhase = ''
-    mkdir -p "$out/libexec/conkeror"
-    cp -r * "$out/libexec/conkeror/"
-
-    makeWrapper "$firefox/bin/firefox" "$out/bin/conkeror" \
-      --add-flags "-app $out/libexec/conkeror/application.ini"
-  '';
-}
+nixpkgs1609.conkeror
