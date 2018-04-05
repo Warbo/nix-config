055c9b483389a2c9753ad96a5cf58b9590c3f86c
Chris Warburton <chriswarbo@gmail.com>
2018-04-05 19:32:20 +0100

    Bump cabal2nix

--- custom/local/cabal2nix.nix
+++ custom/local/cabal2nix.nix
@@ -1,7 +1,7 @@
-{ nixpkgs1603, stable, super }:
+{ nixpkgs1709, stable, super }:
 
 # We use cabal2nix in our Haskell overrides, so we need to use super instead of
 # self, to prevent infinite recursion
 if stable
-   then nixpkgs1603.haskellPackages.cabal2nix
+   then nixpkgs1709.haskellPackages.cabal2nix
    else super.haskellPackages.cabal2nix
