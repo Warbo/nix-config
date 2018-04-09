ee0cc6425442d75f7eaa2cb91360d8d795e29747
Chris Warburton <chriswarbo@gmail.com>
2018-04-09 10:02:15 +0100

    nixpkgs1709 is now default

--- custom/local/warbo-utilities.nix
+++ custom/local/warbo-utilities.nix
@@ -1,19 +1,9 @@
-{ pkgHasBinary, nixpkgs1709, repo, repoSource, runCommand, self,
-  withLatestGit, forceLatest ? false }:
+{ pkgHasBinary, repoSource, self, withLatestGit, forceLatest ? false }:
 
-with rec {
-  version = import (runCommand "nixpkgs-version.nix" { inherit repo; } ''
-    V=$(cat "$repo/.version")
-    echo "\"$V\"" > "$out"
-  '');
-
-  nixPkgs = self // (if version == "17.09"
-                        then {}
-                        else { inherit (nixpkgs1709) ipfs; });
-
+with {
   pkg = withLatestGit {
     url      = "${repoSource}/warbo-utilities.git";
-    srcToPkg = src: import "${src}" { inherit nixPkgs; };
+    srcToPkg = src: import "${src}" { nixPkgs = self; };
     stable   = {
       rev        = "e806837";
       sha256     = "05g1kqaqymnnnfxx516jilz5a9salhv5ggcppfi0bbmmd7i1jy8g";
