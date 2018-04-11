1a2ea2de9667379ea972d77ec96205d34634f31e
Chris Warburton <chriswarbo@gmail.com>
2018-04-11 18:27:44 +0100

    Propagate Haskell deps out to pandocPkgs
    When we import from a derivation, the result doesn't have an explicit dependency
    on that derivation. This is useful, since it lets us do things like checking the
    HEAD of a git repo, whilst still being able to use cached versions if there's
    been no change (if the resulting derivation *did* depend on the repo-checking
    derivation its revision string came from, we would have to rebuild due to a
    cache miss).
    
    For something like checking a git repo this is fine, since it's relatively
    quick. For something like running cabal2nix on all of a package's dependencies
    it would be nice to cache them. Especially since those runCabal2nix derivations
    may be the only things which depend on our Hackage snapshot, and we *really*
    don't want that to get garbage collected when we still want to import things
    from it.
    
    For this reason, the various "haskellPkg*.nix" scripts now pass along the
    derivations that they've imported, which they call "gcRoots".
    
    The "pandocPkgs" package looks like the only thing to be using this
    infrastructure at the moment, but in any case it now adds these gcRoots to its
    own dependencies using 'withDeps'. Since pandocPkgs itself is just a link farm,
    this doesn't add much of a burden due to rebuilds.

--- custom/local/pandocPkgs.nix
+++ custom/local/pandocPkgs.nix
@@ -1,52 +1,54 @@
 # Fixed versions of pandoc, panpipe, panhandle, pandoc-citeproc and dependencies
-{ haskell, haskellPkgsDeps, latestGit, lib, repoSource, runCommand }:
+{ haskell, haskellPkgsDeps, latestGit, lib, repoSource, runCommand, withDeps }:
 
 with lib;
-with rec {
-  hSet = haskellPkgsDeps {
-    deps = [
-      "base >= 4.8"
-      "pandoc-citeproc == 0.10.4"
-      "panpipe == 0.2.0.0"
-      "panhandle == 0.3.0.0"
-      "aeson == 0.11.3.0"
-      "attoparsec == 0.13.0.2" #"attoparsec == 0.13.1.0" seems to have undeclared deps on fail and semigroups
-      /*"fail == 4.9.0.0"
-      "semigroups == 0.18.3"*/
-      "tasty == 0.11.2.1"
-      "lazysmallcheck2012"
-      "pandoc"
-    ];
+with haskellPkgsDeps {
+  deps = [
+    "base >= 4.8"
+    "pandoc-citeproc == 0.10.4"
+    "panpipe == 0.2.0.0"
+    "panhandle == 0.3.0.0"
+    "aeson == 0.11.3.0"
+    "attoparsec == 0.13.0.2"
+    "tasty == 0.11.2.1"
+    "lazysmallcheck2012"
+    "pandoc"
+  ];
 
-    extra-sources = [
-      haskell.packages.ghc7103.lazysmallcheck2012.src
-      (latestGit {
-        url    = "${repoSource}/panhandle.git";
-        stable = {
-          rev    = "7e44d75";
-          sha256 = "1cgk5wslbr507fmh1fyggvk15lipa8x815392j9qf4f922iifdzn";
-        };
-      })
-    ];
+  extra-sources = [
+    haskell.packages.ghc7103.lazysmallcheck2012.src
+    (latestGit {
+      url    = "${repoSource}/panhandle.git";
+      stable = {
+        rev    = "7e44d75";
+        sha256 = "1cgk5wslbr507fmh1fyggvk15lipa8x815392j9qf4f922iifdzn";
+      };
+    })
+  ];
 
-    hsPkgs = haskell.packages.ghc7103;
+  hsPkgs = haskell.packages.ghc7103;
 
-    useOldZlib = true;
-  };
-
-  wanted = hSet.ghcWithPackages (h: [
-    h.pandoc
-    h.panpipe
-    h.pandoc-citeproc
-    h.panhandle
-  ]);
+  useOldZlib = true;
 };
 
-runCommand "pandocPkgs" { inherit wanted; } ''
-  # Pluck out the binaries we want, ignore those we don't (e.g. ghc)
-  mkdir -p "$out/bin"
-  for P in pandoc pandoc-citeproc panhandle panpipe
-  do
-    ln -s "$wanted/bin/$P" "$out/bin/$P"
-  done
-''
+# Add the "gcRoots" as dependencies; these are derivations we imported in order
+# to generate the required Haskell packages, but which aren't actually included
+# in the dependencies of anything. Notably this includes hackageDb. By adding
+# these as dependencies here, we ensure the GC sees them as live.
+withDeps gcRoots (runCommand "pandocPkgs"
+  {
+    wanted = hsPkgs.ghcWithPackages (h: [
+      h.pandoc
+      h.panpipe
+      h.pandoc-citeproc
+      h.panhandle
+    ]);
+  }
+  ''
+    # Pluck out the binaries we want, ignore those we don't (e.g. ghc)
+    mkdir -p "$out/bin"
+    for P in pandoc pandoc-citeproc panhandle panpipe
+    do
+      ln -s "$wanted/bin/$P" "$out/bin/$P"
+    done
+  '')
