afad0f55fbefd86cecba2c7d8ed10f3dcc177bba
Chris Warburton <chriswarbo@gmail.com>
2018-04-23 23:01:39 +0100

    AWF tests

--- custom/local/awf.nix
+++ custom/local/awf.nix
@@ -1,14 +1,23 @@
-{ autoconf, automake, fetchFromGitHub, gcc, gtk2, gtk3, pkgconfig, stdenv,
-  widgetThemes }:
+{ autoconf, automake, fetchFromGitHub, gcc, gtk2, gtk3, pkgconfig, runCommand,
+  stdenv, widgetThemes }:
 
-stdenv.mkDerivation {
-  name = "awf";
-  src  = fetchFromGitHub {
-    owner  = "valr";
-    repo   = "awf";
-    rev    = "c937f1b";
-    sha256 = "0jl2kxwpvf2n8974zzyp69mqhsbjnjcqm39y0jvijvjb1iy8iman";
+with {
+  pkg = stdenv.mkDerivation {
+    name = "awf";
+    src  = fetchFromGitHub {
+      owner  = "valr";
+      repo   = "awf";
+      rev    = "c937f1b";
+      sha256 = "0jl2kxwpvf2n8974zzyp69mqhsbjnjcqm39y0jvijvjb1iy8iman";
+    };
+    buildInputs  = [ autoconf automake gcc gtk2 gtk3 pkgconfig ] ++ widgetThemes;
+    preConfigure = "bash autogen.sh";
   };
-  buildInputs  = [ autoconf automake gcc gtk2 gtk3 pkgconfig ] ++ widgetThemes;
-  preConfigure = "bash autogen.sh";
-}
+};
+runCommand "awf-binaries" { inherit pkg; } ''
+  mkdir -p "$out/bin"
+  for B in "$src/bin"/*
+  do
+    cp -s "$B" "$out/bin/"
+  done
+''
