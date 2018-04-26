4dcb0940fb753c72a6c01372cb54b535ebf5a5c3
Chris Warburton <chriswarbo@gmail.com>
2018-04-27 00:22:11 +0100

    Add stableVersion

---
+++ stableVersion.nix
@@ -0,0 +1,5 @@
+# This is the version of nixpkgs we'll default to as 'stable'. Other versions
+# are kept around, in case we want to backport or pin a package to avoid bugs.
+# The reason we keep this as its own file is so we can import it elsewhere to
+# figure out what the "default" version of things should be.
+"nixpkgs1709"
