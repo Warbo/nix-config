9b32f09617e0827b9625dbc083a4ecbf88d8310f
Chris Warburton <chriswarbo@gmail.com>
2018-05-01 15:33:26 +0100

    Allow stable/unstable to be specified when calling ./default.nix

--- default.nix
+++ default.nix
@@ -1,3 +1,7 @@
-{ unstablePath ? <nixpkgs>, args ? {} }:
+{ unstablePath ? <nixpkgs>, stable ? true, args ? {} }:
 
-import unstablePath ({ config = import ./custom.nix unstablePath; } // args)
+import unstablePath ({
+  config = import ./custom.nix (if stable
+                                   then import ./stableVersion.nix
+                                   else unstablePath);
+} // args)
