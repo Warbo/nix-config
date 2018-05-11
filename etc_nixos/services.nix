bce4df9095caf4d960c29d8534eca70239358e8b
Chris Warburton <chriswarbo@gmail.com>
2018-05-11 15:56:24 +0100

    Make keys a polling service, now that the command is idempotent

--- etc_nixos/services.nix
+++ etc_nixos/services.nix
@@ -916,20 +916,14 @@ with rec {
       };
     };
 
-  keys = monitoredService {
+  keys = pollingService {
     name        = "keys";
     description = "Sets up keyboard";
     RestartSec  = "10";
-    isRunning   = findProcess "xcape";
-    shouldRun   = "${coreutils}/bin/true";
-    stop        = wrap {name="keys-stop";paths=[psmisc];script=''
-      #!/usr/bin/env bash
-      killall xcape || true
-    '';};
+    shouldRun   = findProcess "bin/X ";
     start       = wrap {
       name  = "start-keys";
       vars  = { DISPLAY = ":0"; };
-      paths = [ psmisc warbo-utilities xorg.setxkbmap ];
       file  = "${warbo-utilities}/bin/keys";
     };
   };
