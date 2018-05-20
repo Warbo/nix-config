c5e11864eaa4db01c8d5cd36b7763a3bde6823eb
Chris Warburton <chriswarbo@gmail.com>
2018-05-20 02:57:44 +0100

    Finally deleted helpers.nix
    It's given a deprecation warning for ages

--- custom/local/helpers.nix
+++
@@ -1,5 +0,0 @@
-{ forceBuilds, isPath, repoSource, attrsToDirs, dirContaining, dirsToAttrs,
-  mergeDirs, fetchGitHashless, reverse, sanitiseName, suffMatch, repo2npm,
-  withDeps }@args:
-
-builtins.trace "FIXME: Remove reference to 'helpers'" args
