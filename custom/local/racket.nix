a1a30bbc8594544a39b5ba3d140649c1583f319b
Chris Warburton <chriswarbo@gmail.com>
2018-05-23 01:20:03 +0100

    Add racket and racketWithDeps

---
+++ custom/local/racket.nix
@@ -0,0 +1,10 @@
+{ nixpkgs1609, super }:
+
+with builtins;
+rec {
+  pkg = with (tryEval super.racket);
+        if success
+           then trace "WARNING: Racket override wasn't needed" value
+           else nixpkgs1609.racket;
+  tests = [ pkg ];
+}
