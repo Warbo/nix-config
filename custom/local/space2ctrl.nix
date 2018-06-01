d6bd20a2c1f753c20358ce3a9e4712ddc1ebf437
Chris Warburton <chriswarbo@gmail.com>
2018-06-01 18:41:21 +0100

    Patch space2ctrl to avoid triggering a space at start
    The space2ctrl code triggers a keyup/keydown combo for keycode 255 (which it
    remaps to space) during its 'start' method. This causes a space to be sent to
    whichever application is focused. In the best case this is an annoyance, but it
    seems pretty bad if we'll be restarting space2ctrl at arbitrary
    times (e.g. when plugging in a keyboard).
    
    I've added a patch to the derivation which comments-out the offending code. Note
    that the code has the following comment:
    
        // TODO: document why the following event is needed
    
    So presumably it is needed for some reason, but my quick testing of the patched
    version shows that it seems to work fine without it. In any case, I'd call this
    event a bug, regardless of whether it works around some other undocumented bug.

--- custom/local/space2ctrl.nix
+++ custom/local/space2ctrl.nix
@@ -1,4 +1,4 @@
-{ fetchFromGitHub, stdenv, xorg }:
+{ fetchFromGitHub, replace, stdenv, xorg }:
 
 stdenv.mkDerivation {
   name = "space2ctrl";
@@ -9,7 +9,20 @@ stdenv.mkDerivation {
     sha256 = "1kkbrxdqrmrpynnb18xnjm6gbr1mw2851abcjlv0c15d5calfcw7";
   };
   buildInputs = with xorg; [
-    inputproto libX11 libXext libXi libXtst recordproto xinput
+    inputproto libX11 libXext libXi libXtst recordproto replace xinput
   ];
+
+  # Force 'make install' to use $out
   makeFlags = [ "PREFIX=$(out)" ];
+
+  # Avoid triggering a keypress event on startup by commenting out that code
+  pre1  = "// TODO: document why the following event is needed";
+  post1 = "/*";
+  pre2  = "if (!XRecordEnableContext";
+  post2 = "*/ if (!XRecordEnableContext";
+
+  preBuild  = ''
+    replace "$pre1" "$post1" -- Space2Ctrl.cpp
+    replace "$pre2" "$post2" -- Space2Ctrl.cpp
+  '';
 }
