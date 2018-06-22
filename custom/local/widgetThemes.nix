dea679330ca8c18f30c6d41f2f3f451c784c9e22
Chris Warburton <chriswarbo@gmail.com>
2018-06-22 17:18:56 +0100

    Make themes work on nixpkgs 18.03

--- custom/local/widgetThemes.nix
+++ custom/local/widgetThemes.nix
@@ -1,14 +1,23 @@
-{ clearlooks-phenix, e17gtk-theme, gtk2-aurora-engine, gtk_engines,
+{ clearlooks-phenix, customised, e17gtk-theme, gtk2-aurora-engine, gtk_engines,
   gtk-engine-murrine, skulpture, vertex-theme, xfce, zuki-theme }:
 
+with builtins;
 [
   clearlooks-phenix
   e17gtk-theme
   gtk2-aurora-engine
   gtk_engines
   gtk-engine-murrine
-  skulpture  # Qt, but meh
+  skulpture
   vertex-theme
-  (xfce.gtk_xfce_engine.override { withGtk3 = true; })
+
+  ((if customised ? nixpkgs1709
+       then (x: x)
+       else trace ''Warning: No need to check for gtk_xfce_engines now that
+                    nixpkgs < 18.03 doesn't seem to be supported anymore.'')
+   ((xfce.gtk_xfce_engine or xfce.gtk-xfce-engine).override {
+      withGtk3 = true;
+    }))
+
   zuki-theme
 ]
