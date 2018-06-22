d966169dc6fc4702f99a6d5a0ae9382340f4a9da
Chris Warburton <chriswarbo@gmail.com>
2018-06-22 17:13:19 +0100

    Checking for broken Racket doesn't work, so always pin it

--- custom/local/racket.nix
+++ custom/local/racket.nix
@@ -2,9 +2,6 @@
 
 with builtins;
 rec {
-  pkg = with (tryEval super.racket);
-        if success
-           then trace "WARNING: Racket override wasn't needed" value
-           else nixpkgs1609.racket;
+  pkg   = nixpkgs1609.racket;
   tests = pkg;
 }
