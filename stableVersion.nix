8676ab34bf274dbeff8fa8f04435eecb347ebcc4
Chris Warburton <chriswarbo@gmail.com>
2018-06-23 17:25:32 +0100

    Bump default stable version to nixpkgs 18.03

--- stableVersion.nix
+++ stableVersion.nix
@@ -2,4 +2,4 @@
 # are kept around, in case we want to backport or pin a package to avoid bugs.
 # The reason we keep this as its own file is so we can import it elsewhere to
 # figure out what the "default" version of things should be.
-"nixpkgs1709"
+"nixpkgs1803"
