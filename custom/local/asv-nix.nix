20e71ddac19078289e9f8391ab1361d9a44d95e7
Chris Warburton <chriswarbo@gmail.com>
2018-06-25 19:03:40 +0100

    Perform keyhole surgery on asv-nix to ensure Nix dependency gets overridden
    asv-nix depends on a test which checks that we can benchmark an example project.
    Since asv-nix uses Nix, that test's builder relies on 'withNix' to augment the
    environment with appropriate variables, etc. for running Nix commands. Since we
    want asv-nix to stand alone, not as part of nix-config, it doesn't rely on
    withNix being available in the packageOverrides. Instead, it fetches a
    nix-config git commit, imports it and takes withNix from there. This has the
    effect of pinning that Nix package, or at least avoiding any overrides we might
    have (specifically, our disabling of libuv's flaky test suite).
    
    I think it's fine to leave asv-nix as it is for the time being: if a standalone
    use of it runs into problems then we can cross that bridge when we come to it;
    in particular, we might have the callers of 'withNix' pass in dependencies like
    'nix', either directly as a function call or indirectly by e.g. augmenting their
    own dependencies.

--- custom/local/asv-nix.nix
+++ custom/local/asv-nix.nix
@@ -1,13 +1,37 @@
-{ callPackage, hasBinary, latestGit, repoSource, withDeps }:
+{ callPackage, die, hasBinary, latestGit, lib, nix, repoSource, withDeps }:
 
+with builtins;
 with rec {
-  untested = callPackage (latestGit {
+  raw = callPackage (latestGit {
     url    = "${repoSource}/asv-nix.git";
     stable = {
       rev    = "d5af74d";
       sha256 = "1jp5a8p5dzh2vb2s9k2wf3j2l9fcm7l47ydqy8wlrjiyqlc4jw7a";
     };
   }) {};
+
+  inheritNix = lib.overrideDerivation raw (old: {
+    name    = "asv-nix-fixme";
+    example = lib.overrideDerivation old.example (old:
+      with {
+        withoutNix = filter (p: !(lib.hasPrefix "nix" p.name))
+                            old.buildInputs;
+      };
+      assert (1 + length withoutNix) == length old.buildInputs || die {
+        error      = "Removing Nix from buildInputs didn't remove anything";
+        withNix    = map (x: x.name) old.buildInputs;
+        withoutNix = map (x: x.name) withoutNix;
+      };
+      { buildInputs = withoutNix ++ [ nix ]; }
+    );
+  });
+
+  untested = trace
+    ''
+      FIXME: Overriding the nix dependency in asv-nix's example test. This is to
+      ensure our libuv override (with flaky tests disabled) gets used.
+    ''
+    inheritNix;
 
   pkg = withDeps [ (hasBinary untested "asv") ] untested;
 };
