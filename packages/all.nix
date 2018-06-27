1eb8e25dbe20676905dd399ca096c439258dd10b
Chris Warburton <chriswarbo@gmail.com>
2018-06-27 14:41:17 +0100

    Make 'all' take its components as arguments

--- packages/all.nix
+++ packages/all.nix
@@ -1,63 +1,42 @@
-# This one package depends on all of the packages we want in our user profile
-# so we don't need to keep track of everything separately. If you're on NixOS
-# you can make these available system-wide using /etc/nixos/configuration.nix
+# This one package depends on all of the packages we might want in a user
+# profile so we don't need to keep track of everything separately. If you're on
+# NixOS you can make these available using /etc/nixos/configuration.nix
 # If you're using Nix standalone, or want per-user configuration, you can run
 # a command like `nix-env -iA all` to install into your profile.
-{ self }:
+{ abiword, acpi, albert, anonymous-pro-font, arandr, aspell, aspellDicts,
+  audacious, awf, basic, basket, blueman, buildEnv, cmus, compton, dillo,
+  droid-fonts, emacs, firefox, gcalcli, gensgs, gnome3, iotop, kbibtex_full,
+  keepassx, leafpad, lib, lxappearance, mplayer, mu, mupdf,
+  networkmanagerapplet, nixpkgs1709, paprefs, pavucontrol, picard,
+  pidgin-with-plugins, xfce, xorg, trayer, vlc, w3m, widgetThemes, withDeps,
+  xsettingsd }@args:
 
-with self;
+with builtins;
+with lib;
 with rec {
+  nonPackages = [
+    "aspellDicts" "buildEnv" "gnome3" "lib" "widgetThemes" "withDeps" "xfce"
+    "xorg"
+  ];
+
+  extras = concatLists [
+    [ aspellDicts.en ]
+    [ gnome3.gcr ]
+    (with nixpkgs1709; [ conkeror ])
+    widgetThemes
+    (with xfce; [ exo xfce4notifyd ])
+    (with xorg; [ xkill ])
+  ];
+
+  packages = extras ++ map (name: getAttr name args)
+                           (filter (name: !(elem name nonPackages))
+                                   (attrNames args));
   pkg = buildEnv {
     name  = "all";
-    paths = /*widgetThemes ++ */[
-      abiword
-      acpi
-      albert
-      anonymous-pro-font
-      arandr
-      aspell
-      aspellDicts.en
-      audacious
-      #awf
-      basic # Anything useful for scripts should go in here
-      #blueman
-      basket
-      cmus
-      compton
-      #conkeror
-      dillo
-      droid-fonts
-      emacs
-      #firefox
-      gcalcli
-      gensgs
-      iotop
-      kbibtex_full
-      keepassx
-      leafpad
-      #lxappearance
-      mplayer
-      mu
-      mupdf
-
-      # Networking GUI, requires keyring
-      networkmanagerapplet
-      gnome3.gcr
-      paprefs
-      pavucontrol
-      picard
-      pidgin-with-plugins
-      xorg.xkill
-      trayer
-      vlc
-      w3m
-      #xfce.exo
-      #xfce.xfce4notifyd
-      #xsettingsd
-    ];
+    paths = packages;
   };
 
-  tested = /*withDeps [ (hasBinary pkg "firefox") ]*/ pkg;
+  tested = withDeps [ (hasBinary pkg "firefox") ] pkg;
 };
 {
   pkg   = tested;
