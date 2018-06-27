3e7eb419302fbbd4df23a7420b479fb3e7a28bb2
Chris Warburton <chriswarbo@gmail.com>
2018-06-27 13:55:19 +0100

    Bring in all.nix package from nix-config

---
+++ packages/all.nix
@@ -0,0 +1,65 @@
+# This one package depends on all of the packages we want in our user profile
+# so we don't need to keep track of everything separately. If you're on NixOS
+# you can make these available system-wide using /etc/nixos/configuration.nix
+# If you're using Nix standalone, or want per-user configuration, you can run
+# a command like `nix-env -iA all` to install into your profile.
+{ self }:
+
+with self;
+with rec {
+  pkg = buildEnv {
+    name  = "all";
+    paths = /*widgetThemes ++ */[
+      abiword
+      acpi
+      albert
+      anonymous-pro-font
+      arandr
+      aspell
+      aspellDicts.en
+      audacious
+      #awf
+      basic # Anything useful for scripts should go in here
+      #blueman
+      basket
+      cmus
+      compton
+      #conkeror
+      dillo
+      droid-fonts
+      emacs
+      #firefox
+      gcalcli
+      gensgs
+      iotop
+      kbibtex_full
+      keepassx
+      leafpad
+      #lxappearance
+      mplayer
+      mu
+      mupdf
+
+      # Networking GUI, requires keyring
+      networkmanagerapplet
+      gnome3.gcr
+      paprefs
+      pavucontrol
+      picard
+      pidgin-with-plugins
+      xorg.xkill
+      trayer
+      vlc
+      w3m
+      #xfce.exo
+      #xfce.xfce4notifyd
+      #xsettingsd
+    ];
+  };
+
+  tested = /*withDeps [ (hasBinary pkg "firefox") ]*/ pkg;
+};
+{
+  pkg   = tested;
+  tests = tested;
+}
