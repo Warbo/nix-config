7b3e77526528db3bc6d681ff3e6d39a742749406
Chris Warburton <chriswarbo@gmail.com>
2018-06-30 08:56:49 +0100

    Bump helpers

--- overlays.nix
+++ overlays.nix
@@ -10,24 +10,24 @@ map localOrRemote [
   {
     args = {
       url    = http://chriswarbo.net/git/nix-helpers.git;
-      rev    = "46348ac";
-      sha256 = "13wjrli7wv9da4rhcnqh1g8z0nc2x0mq6wli1fmazw9759lky53m";
+      rev    = "66f9a00";
+      sha256 = "0f84hyqslzb56gwc8yrrn8s95nvdfqn0hf6c9i3cng3bsz3yk53v";
     };
     path = /home/chris/Programming/Nix/nix-helpers;
   }
   {
     args = {
       url    = http://chriswarbo.net/git/warbo-packages.git;
-      rev    = "815f02d";
-      sha256 = "0rwilzi7sc0z9cy41ndyi5k5h8p1w73dvgrrn33a4sc7wwjqbxgl";
+      rev    = "c2ea27d";
+      sha256 = "04aif1s3cxk27nybsxp571fmvswy5vbw0prq67y108sb49mm3288";
     };
     path = /home/chris/Programming/Nix/warbo-packages;
   }
   {
     args = {
       url    = http://chriswarbo.net/git/warbo-utilities.git;
-      rev    = "a5c3f48";
-      sha256 = "10d762iais3xsv1v7x9nyzwvzg7flywygs6fakz6vqqq3ls4jk5c";
+      rev    = "30af50d";
+      sha256 = "0v8fr32ivxjmgdgihgrzbsinw642sp8kyqgspaxlqvp4hdmwljx1";
     };
     path = /home/chris/warbo-utilities;
   }
