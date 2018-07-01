e0705d5003b5bc70fd143445c9e051b141d3dcb9
Chris Warburton <chriswarbo@gmail.com>
2018-07-01 21:30:40 +0100

    Take some programs from nixpkgs1709 rather than whatever <nixpkgs> is

--- packages/all.nix
+++ packages/all.nix
@@ -3,25 +3,25 @@
 # NixOS you can make these available using /etc/nixos/configuration.nix
 # If you're using Nix standalone, or want per-user configuration, you can run
 # a command like `nix-env -iA all` to install into your profile.
-{ abiword, acpi, albert, anonymous-pro-font, arandr, aspell, aspellDicts,
+{ /*abiword,*/ acpi, albert, anonymous-pro-font, arandr, aspell, aspellDicts,
   audacious, awf, basic, basket, blueman, buildEnv, cmus, compton, die, dillo,
-  droid-fonts, emacsWithPkgs, firefox, gcalcli, gensgs, gnome3, hasBinary,
-  iotop, kbibtex_full, keepassx, leafpad, lib, lxappearance, mplayer, mu, mupdf,
-  networkmanagerapplet, nixpkgs1709, paprefs, pavucontrol, picard,
-  pidgin-with-plugins, stripOverrides, xfce, xorg, trayer, vlc, w3m,
-  widgetThemes, withDeps, xsettingsd }@args:
+  droid-fonts, emacsWithPkgs, /*firefox,*/ gcalcli, gensgs, gnome3, hasBinary,
+  iotop, kbibtex_full, keepassx, leafpad, lib, lxappearance, /*mplayer,*/ mu,
+  mupdf, nixpkgs1709, paprefs, pavucontrol, picard, pidgin-with-plugins,
+  stripOverrides, xfce, xorg, trayer, /*vlc,*/ w3m, widgetThemes,
+  xsettingsd }@args:
 
 with builtins;
 with lib;
 with rec {
   nonPackages = [
     "aspellDicts" "buildEnv" "die" "gnome3" "hasBinary" "lib" "nixpkgs1709"
-    "stripOverrides" "widgetThemes" "withDeps" "xfce" "xorg"
+    "stripOverrides" "widgetThemes" "xfce" "xorg"
   ];
 
   extras = widgetThemes // {
     inherit (gnome3)      gcr;
-    inherit (nixpkgs1709) conkeror;
+    inherit (nixpkgs1709) abiword conkeror firefox gnumeric mplayer vlc;
     inherit (xfce)        exo xfce4notifyd;
     inherit (xorg)        xkill;
     aspellDicts = aspellDicts.en;
