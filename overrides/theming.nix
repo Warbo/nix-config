a7b9c4dd222d76fcafbbd9336f4d32a1ba46fd85
Chris Warburton <chriswarbo@gmail.com>
2018-07-02 16:25:01 +0100

    Take skulpture from nixpkgs 17.09 since it breaks for 18.03

--- overrides/theming.nix
+++ overrides/theming.nix
@@ -4,7 +4,8 @@ self: super:
   overrides = {
     widgetThemes = {
       inherit (self) clearlooks-phenix e17gtk-theme gtk2-aurora-engine
-        gtk_engines gtk-engine-murrine skulpture vertex-theme zuki-theme;
+        gtk_engines gtk-engine-murrine vertex-theme zuki-theme;
+      inherit (self.customised.nixpkgs1709) skulpture;
     };
   };
   tests = {};
