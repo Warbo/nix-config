8a41656b3587f747add4f2e132f2483eaf0e68f2
Chris Warburton <chriswarbo@gmail.com>
2018-07-09 07:53:26 +0100

    Merge skulpture into themes

--- overrides/theming.nix
+++ overrides/theming.nix
@@ -2,10 +2,9 @@ self: super:
 
 {
   overrides = {
-    widgetThemes = {
+    widgetThemes = self.skulpture // {
       inherit (self) clearlooks-phenix e17gtk-theme gtk2-aurora-engine
         gtk_engines gtk-engine-murrine vertex-theme zuki-theme;
-      inherit (self.customised.nixpkgs1709) skulpture;
     };
   };
   tests = {};
