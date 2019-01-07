7028ef65ea6eaa830622031095eace39e80077fe
Chris Warburton <chriswarbo@gmail.com>
2019-01-07 21:21:03 +0000

    Bump

--- helpers.nix
+++ helpers.nix
@@ -3,8 +3,8 @@
 {
   nix-helpers = fetchgit {
     url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "4fb7c87";
-    sha256 = "1wwagwxjj8v5q5wi75fcqz6xl17b1jf4l5p051vx371xj373v3dk";
+    rev    = "148bd5e";
+    sha256 = "0wywgdmv4gllarayhwf9p41pzrkvgs32shqrycv2yjkwz321w8wl";
   };
 
   warbo-packages = fetchgit {
@@ -15,7 +15,7 @@
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "9c95256";
-    sha256 = "04wz3d8lqj04p9hf0yyh1f437bkkbvg8kj9xff2phyc14hxw0s0j";
+    rev    = "82eb25c";
+    sha256 = "1zjlsxm5fq3xpyqa2xv6dinn05yvmbz3a708l249a8m2wvsadrpd";
   };
 }
