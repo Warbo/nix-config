e949b4f41c786ad24c61bf26518d677a427477ba
Chris Warburton <chriswarbo@gmail.com>
2019-01-13 14:50:03 +0000

    Simplify helpers

--- check.sh
+++ check.sh
@@ -8,18 +8,17 @@ do
     nix-instantiate --parse "$F" > /dev/null
 done
 
+REPO="warbo-utilities"
+echo "Checking $REPO version" 1>&2
+
 # Allow failure to get HEAD (e.g. in case we're offline)
-for REPO in nix-helpers warbo-packages warbo-utilities
-do
-    echo "Checking $REPO version" 1>&2
-    if REV=$(git ls-remote "http://chriswarbo.net/git/$REPO.git" |
-                 grep HEAD | cut -d ' ' -f1 | cut -c1-7)
-    then
-        grep "$REV" < helpers.nix || {
-            echo "Didn't find $REPO rev '$REV' in helpers.nix" 1>&2
-            exit 1
-        }
-        echo "Checking $REPO in helpers.nix builds (e.g. for SHA256)" 1>&2
-        nix-build --no-out-link -A "$REPO" helpers.nix || exit 1
-    fi
-done
+if REV=$(git ls-remote "http://chriswarbo.net/git/$REPO.git" |
+             grep HEAD | cut -d ' ' -f1 | cut -c1-7)
+then
+    grep "$REV" < helpers.nix || {
+        echo "Didn't find $REPO rev '$REV' in helpers.nix" 1>&2
+        exit 1
+    }
+    echo "Checking $REPO in helpers.nix builds (e.g. for SHA256)" 1>&2
+    nix-build --no-out-link -A "$REPO" helpers.nix || exit 1
+fi
--- helpers.nix
+++ helpers.nix
@@ -1,21 +1,13 @@
 { fetchgit ? (import <nixpkgs> { config = {}; overlays = []; }).fetchgit }:
 
-{
-  nix-helpers = fetchgit {
-    url    = http://chriswarbo.net/git/nix-helpers.git;
-    rev    = "5cecd3f";
-    sha256 = "0g4qjciim81wi2hqydmlkxcb1923gaxdln5qx5icyy3639ap6xq3";
-  };
-
-  warbo-packages = fetchgit {
-    url    = http://chriswarbo.net/git/warbo-packages.git;
-    rev    = "e988092";
-    sha256 = "082ibmy2q9zvrm85bncm10v29rm53k25dwlgmqgmldfppprxcwja";
-  };
+rec {
+  inherit (import "${warbo-utilities}/helpers.nix" { inherit fetchgit; })
+    nix-helpers
+    warbo-packages;
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "fd37d11";
-    sha256 = "1vgsiclyd2ksi8n8z94m3dd242bm277sc3pjihl6qrw1nkiia82m";
+    rev    = "1149d23";
+    sha256 = "0l5sg1di029hfd948yhycr07g0d1rf21q6vfhzwcj1jha8xgdari";
   };
 }
