0a3720cb45d5391114da435b40bdfd2dad0d71f7
Chris Warburton <chriswarbo@gmail.com>
2019-02-14 17:30:07 +0000

    Bump for artemis changes

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "443cee4";
-    sha256 = "150agcw9652z19px9fxj03qrs11l4m05n8svzlxkqyfbnqa2fvjd";
+    rev    = "eb0f1b5";
+    sha256 = "0kzadxikamh9ff37px8kn30hbwwgymb39prrkhifwnnqcx5mzlvv";
   };
 }
