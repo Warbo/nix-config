5f7d05323da11cbd3904af78659de7d92e121230
Chris Warburton <chriswarbo@gmail.com>
2019-04-02 12:36:32 +0100

    Bump warbo-packaged for markRead during fetch

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "7c56211";
-    sha256 = "0v6l6jk689n2f36qj4gq90bc5nnpcp46fd0i23pb25wgkxmm875n";
+    rev    = "b00d1e1";
+    sha256 = "1ixl1z8w1mnaw8vc1912n3jxl6qfs1k07x1qdv30qv3n0wa1ds5j";
   };
 }
