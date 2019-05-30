def2910d9a8fcbdf30917a5f0291d973d497d829
Chris Warburton <chriswarbo@gmail.com>
2019-05-30 18:07:40 +0100

    Bump helpers for fixes and stuff

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "1e812d0";
-    sha256 = "1wpp3mpa009arw65phk83zyjzc3l1fwq02bn2a9zsrgw73g0n9fd";
+    rev    = "ee9f528";
+    sha256 = "1r72gjcphwa7qmcip0pmyddirp651xxq8dk1jhr309s0my7g7qmi";
   };
 }
