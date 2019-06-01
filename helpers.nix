9025d280c5477d4af6e339c9efba6e0ebb4b0b22
Chris Warburton <chriswarbo@gmail.com>
2019-06-01 02:29:04 +0100

    Bump for gcalcli fix

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "1869df1";
-    sha256 = "0plwfms3gnvp40bgr3gpj1y72sg57q2b3kzsxih3b6yk1qdc863h";
+    rev    = "517befa";
+    sha256 = "1nbvllbl14dn0cvgxywj7lj9kzl9icv61db6qbb1f2xvgxzpmqb3";
   };
 }
