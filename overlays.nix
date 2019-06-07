8aa80901639be4323a4cd0dd2607967d092d2152
Chris Warburton <chriswarbo@gmail.com>
2019-06-07 18:13:14 +0100

    Stick to published repos

--- overlays.nix
+++ overlays.nix
@@ -1,20 +1,7 @@
 with builtins;
 with rec {
-  localOrRemote = name:
-    with rec {
-      file = "overlay.nix";
-      path = locals."${name}" + "/${file}";
-    };
-    import (if pathExists path
-               then path
-               else "${remotes."${name}"}/${file}");
-
-  remotes = import ./helpers.nix {};
-
-  locals  = {
-    nix-helpers     = /home/chris/Programming/Nix/nix-helpers;
-    warbo-packages  = /home/chris/Programming/Nix/warbo-packages;
-    warbo-utilities = /home/chris/warbo-utilities;
-  };
+  remotes       = import ./helpers.nix {};
+  importOverlay = name: import "${getAttr name remotes}/overlay.nix";
 };
-map localOrRemote (attrNames (locals // remotes)) ++ [ (import ./overlay.nix) ]
+map importOverlay [ "nix-helpers" "warbo-packages" "warbo-utilities" ] ++
+    [ (import ./overlay.nix) ]
