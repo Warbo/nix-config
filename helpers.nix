6347d9911ff7cacdad043169d8e0fe78267ccd42
Chris Warburton <chriswarbo@gmail.com>
2019-07-11 14:36:49 +0100

    Bump for dodgy_feeds

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "90ff43b";
-    sha256 = "0h031m1z78chb58bl90z9yaly58nqinkxj1d06y36b94c6d969a1";
+    rev    = "66d964e";
+    sha256 = "0zxxpf5a3hjfjjqi4nrfbdlkrqn2jpcg98v0affrkbmx9piy1w8h";
   };
 }
