675e4b6781e9380715610ffea0aef9f6988a1583
Chris Warburton <chriswarbo@gmail.com>
2019-07-29 22:06:28 +0100

    Bump for newer get_iplayer

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "3b80391";
-    sha256 = "07gl6fqfjlg5nj12pgjqavx9apkql30ifpaqw9r26jrh90s5k9qk";
+    rev    = "f3fdbb6";
+    sha256 = "1rc0law882dd8d2mwdw51xfrrym6az7f2cbbsmazl8hxhrri54kj";
   };
 }
