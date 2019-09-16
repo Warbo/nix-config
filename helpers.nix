bcdb665a37e8c9c918c2ccc2a8e44f6924e542e6
Chris Warburton <chriswarbo@gmail.com>
2019-09-16 12:30:08 +0100

    Bump utilities
    For copy fix

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "8a7e069";
-    sha256 = "0q6lnr4mrah1b67iq1i7022cls65hrs3wqabv308wn85y9sz7rzv";
+    rev    = "2dd4193";
+    sha256 = "14smf7w5xmqa7847f0dk5pfn1bsvbcmhs67dap61irhsnssb06cw";
   };
 }
