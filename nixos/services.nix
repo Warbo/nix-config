8d8b7a10e96ae555b8a927144c1cf0e971825965
Chris Warburton <chriswarbo@gmail.com>
2020-02-08 00:41:42 +0000

    Formatting

--- nixos/services.nix
+++ nixos/services.nix
@@ -139,9 +139,9 @@ with rec {
     ln -s "${config.security.wrapperDir}/sudo" "$out/bin/sudo"
   '';
 
-  pingOnce  = "${config.security.wrapperDir}/ping -c 1";
+  pingOnce = "${config.security.wrapperDir}/ping -c 1";
 
-  online    = "${pingOnce} google.com 1>/dev/null 2>/dev/null";
+  online   = "${pingOnce} google.com 1>/dev/null 2>/dev/null";
 };
 {
   thermald-nocheck = mkService {
