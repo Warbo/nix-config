55d463e1c53a55b3b029380ddfe77faed4df0840
Chris Warburton <chriswarbo@gmail.com>
2020-02-08 00:41:02 +0000

    Bump

--- helpers.nix
+++ helpers.nix
@@ -7,7 +7,7 @@ rec {
 
   warbo-utilities = fetchgit {
     url    = http://chriswarbo.net/git/warbo-utilities.git;
-    rev    = "8d5f71c";
-    sha256 = "06x5z0zi0b2rypvdklkxp0r49ki2rb6jz446lwfvifph19vgrs1w";
+    rev    = "44d0777";
+    sha256 = "01hb510p0x16j8z5fswcs79y9mwizgy242qa2yb1jmwnjrsw8xpg";
   };
 }
