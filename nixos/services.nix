95dc5555940ad054df8a5789c6ec731fe6e031b3
Chris Warburton <chriswarbo@gmail.com>
2021-03-30 03:23:36 +0100

    Use inboxen from warbo-utilities

--- nixos/services.nix
+++ nixos/services.nix
@@ -298,52 +298,12 @@ with rec {
       RestartSec = 600;
       ExecStart  = wrap {
         name   = "inboxen-start";
-        paths  = [ bash coreutils iputils isync mu procps psutils gnused ];
+        paths  = [ bash ];
         script = ''
           #!${bash}/bin/bash
           set -e
           ${online} || exit
-          CODE=0
-
-          echo "Fetching mail" 1>&2
-          for INBOX in gmail dundee
-          do
-            if timeout -s 9 1800 mbsync --verbose "$INBOX"
-            then
-              echo "Finished syncing $INBOX" 1>&2
-            else
-              echo "Error syncing $INBOX" 1>&2
-              CODE=1
-            fi
-          done
-
-          # Try waiting for existing mu processes to die
-          for RETRY in $(seq 1 20)
-          do
-            # Find running mu processes. Try to exclude mupdf, etc.
-            if P=$(ps auxww | grep '[ /]mu\( \|$\)')
-            then
-              echo "Stopping running mu instances" 1>&2
-              echo "$P" | sed -e 's/  */ /g' | cut -d ' ' -f2 | while read -r I
-              do
-                kill -INT "$I"
-              done
-              sleep 1
-            else
-              # Stop early if nothing's running
-              break
-            fi
-          done
-
-          echo "Indexing maildirs for Mu" 1>&2
-          if mu index --maildir=~/Mail --lazy-check
-          then
-            echo "Finished indexing" 1>&2
-          else
-            echo "Error indexing" 1>&2
-            CODE=2
-          fi
-          exit "$CODE"
+          ${warbo-utilities}/bin/inboxen
         '';
       };
     };
