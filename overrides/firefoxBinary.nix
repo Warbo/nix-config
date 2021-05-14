9082325487de68d688955f19e31bbf5e0ccca0df
Chris Warburton <chriswarbo@gmail.com>
2021-05-14 15:06:31 +0100

    Force Firefox source path to end in .tar.bz2

--- overrides/firefoxBinary.nix
+++ overrides/firefoxBinary.nix
@@ -1,10 +1,19 @@
-self: super: {
+self: super:
+with { inherit (self.nix-config-sources) firefox; };
+{
   overrides = {
-    firefoxBinary = self.makeFirefoxBinary self.nix-config-sources.firefox;
+    firefoxBinary = self.makeFirefoxBinary (
+      # Force output path to end in .tar.bz2 so it unpacks properly
+      self.runCommand
+        "firefox-src-${firefox.version}.tar.bz2"
+        { raw = firefox.outPath; }
+        ''ln -s "$raw" "$out"'' //
+      { inherit (firefox) version; }
+    );
   };
   checks = super.lib.mapAttrs self.nix-config-version-check {
     firefoxBinary = {
-      inherit (self.nix-config-sources.firefox) version;
+      inherit (firefox) version;
       url    = https://www.mozilla.org/en-US/firefox/releases;
       script = ''
         grep -o 'data-latest-firefox="[^"]*"' < "$page" |
