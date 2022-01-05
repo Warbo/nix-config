84f96dde087ba686d4c611e493a12f7d600ab083
Chris Warburton <chris@zipabout.com>
2022-01-05 21:31:51 +0000

    Use immutable users on pinephone

--- nixos/modules/machines/pinephone.nix
+++ nixos/modules/machines/pinephone.nix
@@ -1,3 +1,6 @@
 { config, lib, pkgs, ... }: {
   networking.hostname = "pinephone";
+  # We always want immutable users, but we set it per-machine since it wasn't
+  # set on thinkpad (and I don't know if it's safe to enable post-hoc?)
+  users.mutableUsers = false;
 }
