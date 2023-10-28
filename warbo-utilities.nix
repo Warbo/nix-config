b5732765c8436eb674b4c96eb4cb3a4974f63241
Chris Warburton <chriswarbo@gmail.com>
2023-10-28 02:30:12 +0100

    bump warbo-utilities

--- warbo-utilities.nix
+++ warbo-utilities.nix
@@ -2,5 +2,5 @@ import (fetchGit {
   name = "warbo-utilities";
   url = "http://chriswarbo.net/git/warbo-utilities.git";
   ref = "master";
-  rev = "4176ae9bfe0a026be3ee17a18a5a1b2bf66a31c4";
+  rev = "7cfeb376db95b8ebe7f69ef9a52c0213740ea431";
 }) { }
