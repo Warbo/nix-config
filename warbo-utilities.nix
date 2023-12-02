aa598ddd9f8ac4c4fadbf1d932f5560364ff6fe0
Chris Warburton <chriswarbo@gmail.com>
2023-12-02 18:58:23 +0000

    warbo-utilities: bump for nixpkgs 23.11

--- warbo-utilities.nix
+++ warbo-utilities.nix
@@ -2,5 +2,5 @@ import (fetchGit {
   name = "warbo-utilities";
   url = "http://chriswarbo.net/git/warbo-utilities.git";
   ref = "master";
-  rev = "ab220ed8f04d76a67896263d0f173a5d52a9f2f6";
+  rev = "324dd9e1394928db35e7acc79995164d9cbd2aa4";
 }) { }
