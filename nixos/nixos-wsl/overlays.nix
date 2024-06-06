376ad53d75e36273c568c80cf35f046c7c7d5f35
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-06 21:00:18 +0000

    WSL: include warbo-packages

--- nixos/nixos-wsl/overlays.nix
+++ nixos/nixos-wsl/overlays.nix
@@ -1,4 +1,5 @@
 { nix-helpers-src ? sources.nix-helpers
+, warbo-packages-src ? sources.warbo-packages
 , sources ? import ../../nix/sources.nix
 }:
 # TODO: Make the nix-config overlays a bit more direct to use
@@ -6,6 +7,12 @@
   (self: super: {
     # Repo of helper functions
     nix-helpers = import nix-helpers-src { nixpkgs = super; };
+
+    # Repo of useful packages
+    warbo-packages = import warbo-packages-src {
+      inherit (self) nix-helpers;
+      nixpkgs = super;
+    };
   })
   (self: super:
     # Use some of this repo's overrides. TODO: Update them to use options or
