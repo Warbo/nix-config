1677083cabe982cf934e15f6b4c3171381a62764
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-06 20:59:55 +0000

    Don't need fork of update-nix-fetchgit when using fetchFromGitHub

--- nixos/nixos-wsl/overlays.nix
+++ nixos/nixos-wsl/overlays.nix
@@ -4,13 +4,6 @@
 # TODO: Make the nix-config overlays a bit more direct to use
 [
   (self: super: {
-    # GitHub don't support 'v3' in their API anymore; this fork uses an
-    # appropriate replacement.
-    update-nix-fetchgit = super.update-nix-fetchgit.overrideAttrs (old: {
-      src = builtins.fetchTarball {
-        url = "https://github.com/ja0nz/update-nix-fetchgit/archive/7460aede467fbaf4f3db363102d299232f9684e2.tar.gz";
-      };
-    });
     # Repo of helper functions
     nix-helpers = import nix-helpers-src { nixpkgs = super; };
   })
