d5688fb3e1d674e42f022cec9d4d18ef854fc630
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-06 20:59:30 +0000

    Take devCli from metaPackages

--- nixos/nixos-wsl/overlays.nix
+++ nixos/nixos-wsl/overlays.nix
@@ -1,3 +1,8 @@
+{ nix-helpers-src ? sources.nix-helpers
+, sources ? import ../../nix/sources.nix
+}:
+# TODO: Make the nix-config overlays a bit more direct to use
+[
   (self: super: {
     # GitHub don't support 'v3' in their API anymore; this fork uses an
     # appropriate replacement.
@@ -6,4 +11,17 @@
         url = "https://github.com/ja0nz/update-nix-fetchgit/archive/7460aede467fbaf4f3db363102d299232f9684e2.tar.gz";
       };
     });
+    # Repo of helper functions
+    nix-helpers = import nix-helpers-src { nixpkgs = super; };
   })
+  (self: super:
+    # Use some of this repo's overrides. TODO: Update them to use options or
+    # something, so we can opt-in to what we want more directly.
+    with rec {
+      slf = sup // extra.overrides;
+      sup = super // super.nix-helpers // super.warbo-packages;
+      extra = import ../../overrides/metaPackages.nix slf sup;
+    };
+    { inherit (extra.overrides) devCli; })
+]
+
