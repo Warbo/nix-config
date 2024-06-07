e84dac46c77c551b337e2bfa91be2b3d5a18cbc0
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-07 15:08:27 +0000

    check.sh: check formatting, and reformat if necessary

--- check.sh
+++ check.sh
@@ -2,12 +2,20 @@
 set -e
 
 # Simple, quick sanity check. Useful as a git pre-commit hook.
-find . -name "*.nix" | while read -r F
+while read -r F
 do
-    echo "Checking '$F'" 1>&2
-    nix-instantiate --parse "$F" > /dev/null
-done
+    [[ -n "$DEBUG" ]] && echo "Checking '$F'" 1>&2
+    nix-instantiate --parse "$F" > /dev/null || CODE=1
+    if command -v nixfmt > /dev/null
+    then
+        nixfmt -w 80 -c "$F" || CODE=1
+        [[ -n "$REFORMAT" ]] && nixfmt -w 80 "$F"
+    fi
+done < <(find . -name "*.nix")
 
 # Fail if checks don't pass
 EXPR='(import ./nix).nix-config-check || abort "Checks failed"'
-nix-instantiate --show-trace --read-write-mode --eval -E "$EXPR" > /dev/null
+nix-instantiate --show-trace --read-write-mode --eval -E "$EXPR" > /dev/null ||
+    CODE=1
+
+exit "$CODE"
