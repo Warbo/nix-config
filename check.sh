165b8ff49288c53ddb9658b9e5e0c9dd9d3fb544
Chris Warburton <Chris.Warburton@tracsis.com>
2024-06-08 10:26:04 +0000

    improve check.sh

--- check.sh
+++ check.sh
@@ -2,14 +2,23 @@
 set -e
 
 # Simple, quick sanity check. Useful as a git pre-commit hook.
+CODE=1
 while read -r F
 do
     [[ -n "$DEBUG" ]] && echo "Checking '$F'" 1>&2
     nix-instantiate --parse "$F" > /dev/null || CODE=1
     if command -v nixfmt > /dev/null
     then
-        nixfmt -w 80 -c "$F" || CODE=1
-        [[ -n "$REFORMAT" ]] && nixfmt -w 80 "$F"
+        if ! nixfmt -w 80 -c "$F"
+        then
+            CODE=1
+            if [[ -n "$REFORMAT" ]]
+            then
+                nixfmt -w 80 "$F"
+            else
+                echo "(Set REFORMAT=1 to auto-format)" 1>&2
+            fi
+        fi
     fi
 done < <(find . -name "*.nix")
 
