0ebf21d6e2fbb7c00c0848331821ffa2d09b1b43
Chris Warburton <chriswarbo@gmail.com>
2024-06-11 16:00:31 +0100

    home.nix: remove old Home Manager commit (for now)

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -256,16 +256,7 @@ with rec {
     };
 
     home-manager = {
-      path = fetchTarball {
-        sha256 = "sha256:16078fwcmqq41dqfnm124xxm8l6zykvqlj1kzgi0fvfil4y86slm";
-        url = pkgs.lib.concatStringsSep "/" [
-          "https://github.com"
-          "nix-community"
-          "home-manager"
-          "archive"
-          "release-23.11.tar.gz"
-        ];
-      };
+      path = null; #import ./nixos-import.nix;
     };
 
     /* TODO: Add these:
