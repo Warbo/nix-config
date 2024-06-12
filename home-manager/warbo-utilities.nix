5480aaf1ec04ef8cd37003f6b6415821f0e2678b
Chris Warburton <chriswarbo@gmail.com>
2024-06-12 09:16:07 +0100

    Bump warbo-utilities

--- home-manager/warbo-utilities.nix
+++ home-manager/warbo-utilities.nix
@@ -1,6 +1,7 @@
+# TODO: Use sources; this is left over from dotfiles
 import (fetchGit {
   name = "warbo-utilities";
   url = "http://chriswarbo.net/git/warbo-utilities.git";
   ref = "master";
-  rev = "868153917daae3305db7fb363178b18dd847d758";
+  rev = "31e7385999df4a6fe981e4458339ac7a8380edf2";
 }) { }
