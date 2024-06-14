d7fedded35bd4a9bce430db066a5bf5aadd46e12
Chris Warburton <chriswarbo@gmail.com>
2024-06-14 17:57:21 +0100

    Comment

--- nixos/nixos-amd64/configuration.nix
+++ nixos/nixos-amd64/configuration.nix
@@ -1,7 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
+# AMD64 laptop
 {
   config,
   lib,
