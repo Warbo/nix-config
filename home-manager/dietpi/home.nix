1d1aa3fd740159946a96e3f9685858ca3699c759
Chris Warburton <chriswarbo@gmail.com>
2024-06-27 15:27:23 +0100

    dietpi: add example service

--- home-manager/dietpi/home.nix
+++ home-manager/dietpi/home.nix
@@ -26,11 +26,51 @@
     # '';
   };
 
-  home.sessionVariables = {
-    #NIX_PATH = "home-manager=${}"
-  };
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   programs.home-manager.path = import ../nixos-import.nix;
+
+  systemd.user = {
+    services = {
+      fetch-youtube-feeds = {
+        Unit.Description = "Fetch Youtube feeds";
+        Service = {
+          Type = "oneshot";
+          RemainAfterExit = "no";
+          ExecStart = "${pkgs.writeShellScript "fetch-youtube-feeds" ''
+            set -ex
+            true
+            # TODO: Set location we're downloading feeds to and putting maildirs
+
+            # TODO: Loop through feeds in random order, downloading them.
+            # extracting the video URLs and mv'ing them into a pending dir
+            #while read -r LINE
+            #do
+            #  NAME=$(echo "$LINE" | cut -f1)
+            #  URL=$(echo "$LINE" | cut -f2)
+            #  mkdir
+            #done < <(shuf < "$HOME/yt")
+          ''}";
+        };
+      };
+
+      # TODO: Trigger on path, pointing at pending URL dir:
+      #  - We only want one copy running; but triggered whenever new URLs appear
+      #  - Loop through pending URLs in random order, one at a time
+      #  - Try downloading with yt-dlp, into an unfinished downloads dir
+      #  - Wait until finished, ignoring common failures (shorts, etc.)
+      #  - Once finished, mv resulting file to sub-folder of TODO
+      #  - If successful, or known failure, delete URL from pending dir
+      #fetch-youtube-files = {};
+    };
+    timers = {
+      fetch-youtube-feeds = {
+        Unit.Description = "Fetch Youtube feeds daily";
+        Timer = {
+          OnBootSec = "15min";
+          OnUnitActiveSec = "1d";
+        };
+      };
+    };
+  };
 }
