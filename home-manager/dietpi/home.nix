4a73ec5ce14def4fe791cb7cb3e269dba1e812a6
Chris Warburton <chriswarbo@gmail.com>
2024-06-29 23:07:44 +0100

    dietpi: implement youtube-fetching

--- home-manager/dietpi/home.nix
+++ home-manager/dietpi/home.nix
@@ -35,9 +35,9 @@
     git.userEmail = "chriswarbo@gmail.com";
   };
 
-  systemd.user = {
+  systemd.user = with { yt-dir = builtins.toString ~/youtube; }; {
     systemctlPath = "/bin/systemctl"; # Use native, since Nix one hangs
-    services = with { yt-dir = builtins.toString ~/youtube; }; {
+    services = {
       # TODO: Move this to a standalone module, to avoid clutter
       fetch-youtube-feeds = {
         Unit.Description = "Fetch Youtube feeds";
@@ -45,7 +45,9 @@
           Type = "oneshot";
           RemainAfterExit = "no";
           ExecStart = "${pkgs.writeShellScript "fetch-youtube-feeds" ''
-            set -ex
+            set -e
+            mkdir -p ${yt-dir}/temp
+
             # Fetch feeds in random order, in case one causes breakage
             < ${yt-dir}/feeds.tsv shuf | while read -r LINE
             do
@@ -56,8 +58,8 @@
               echo "Processing $LINE" 1>&2
 
               # Video IDs will go in here
-              TODO=${yt-dir}/todo/"$NAME"
-              DONE=${yt-dir}/done/"$NAME"
+              TODO=${yt-dir}/todo
+              DONE=${yt-dir}/done
               mkdir -p "$TODO"
               mkdir -p "$DONE"
 
@@ -65,7 +67,12 @@
               while read -u 3 -r VURL
               do
                 VID=$(echo "$VURL" | cut -d= -f2)
-                [[ -e "$DONE/$VID" ]] || echo "$VURL" > "$TODO/$VID"
+                [[ -e "$DONE/$VID" ]] || {
+                  # Write atomically to TODO
+                  mkdir -p ${yt-dir}/temp/"$VID"
+                  echo "$VURL" > ${yt-dir}/temp/"$VID"/"$NAME"
+                  mv ${yt-dir}/temp/"$VID" "$TODO/"
+                }
               done 3< <(curl "$URL" |
                 grep -o 'https://www.youtube.com/watch?v=[^<>" &]*')
             done
@@ -80,7 +87,77 @@
       #  - Wait until finished, ignoring common failures (shorts, etc.)
       #  - Once finished, mv resulting file to sub-folder of TODO
       #  - If successful, or known failure, delete URL from pending dir
-      #fetch-youtube-files = {};
+      fetch-youtube-files = {
+        Unit.Description = "Fetch all Youtube videos identified in todo";
+        Service = {
+          Type = "oneshot";
+          RemainAfterExit = "no";
+          ExecStart = "${pkgs.writeShellScript "fetch-youtube-files" ''
+            set -e
+            mkdir -p ${yt-dir}/fetched
+
+            while true
+            do
+              # Run find to completion before changing anything in todo
+              FILES=$(find ${yt-dir}/todo -type f | shuf)
+
+              # Stop if nothing more was found
+              echo "$FILES" | grep -q '^.' || break
+
+              while read -r F
+              do
+                # Extract details
+                URL=$(cat "$F")
+                NAME=$(basename "$F")
+                VID=$(basename "$(dirname "$F")")
+
+                # Set up a temp dir to work in. The name is based on the VID; so
+                # we can tell if this entry has been attempted before.
+                T=${yt-dir}/temp/fetch-"$VID"
+                if [[ -e "$T" ]]
+                then
+                  echo "Skipping $VID as $T already exists (prev. failure?)" >&2
+                  continue
+                fi
+
+                # If this hasn't been attempted yet, make a working dir inside
+                # the temp dir, named after the destination directory (making it
+                # easier to move atomically without overlaps). Metadata is kept
+                # in the temp dir, so we can tell what happened.
+                mkdir -p "$T/$NAME"
+                pushd "$T/$NAME"
+                  if ${pkgs.yt-dlp}/bin/yt-dlp -f 'b[height<600]' "$URL" \
+                       1> >(tee ../stdout)
+                       2> >(tee ../stderr 1>&2)
+                  then
+                    touch ../success
+                  fi
+                popd
+
+                # If the fetch succeeded, move the result atomically to fetched
+                # and move the VID from todo to done
+                if [[ -e "$T/success" ]]
+                then
+                  mv "$T" ${yt-dir}/fetched/
+                  mkdir -p ${yt-dir}/done/"$NAME"
+                  mv "$F" ${yt-dir}/done/"$NAME"/"$VID"
+                  rmdir "$(dirname "$F")"
+                fi
+
+                sleep 10 # Slight delay to cut down on spam
+              done < <(echo "$FILES")
+
+              sleep 10  # Slight delay to cut down on spam
+            done
+          ''}";
+        };
+      };
+    };
+    paths = {
+      fetch-youtube-files = {
+        Unit.Description = "Fetch files when new entries appear in todo";
+        Path.DirectoryNotEmpty = "${yt-dir}/todo";
+      };
     };
     timers = {
       fetch-youtube-feeds = {
