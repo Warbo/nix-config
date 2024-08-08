6faf22750bbc3b8b1513df0e4543cfe3db341851
Chris Warburton <chriswarbo@gmail.com>
2024-08-08 15:50:00 +0100

    yt-dlp: put revision in name, so we can tell them apart

--- overrides/yt-dlp.nix
+++ overrides/yt-dlp.nix
@@ -1,14 +1,16 @@
 self: super: {
   overrides = {
-    yt-dlp = self.nixpkgsUpstream.yt-dlp.overrideAttrs (_: {
-      name = "yt-dlp-custom";
-      src = super.fetchFromGitHub {
-        owner = "yt-dlp";
-        repo = "yt-dlp";
-        rev = "abe10131fc235b7cc7af39f833e417f4264c1fdb";
-        sha256 = "sha256-u069kH4DsOLwSC7DrXkS0pOSmaYDHd9EwsH/6FirBZI=";
-      };
-    });
+    yt-dlp =
+      with { rev = "abe10131fc235b7cc7af39f833e417f4264c1fdb"; };
+      self.nixpkgsUpstream.yt-dlp.overrideAttrs (_: {
+        name = "yt-dlp-${rev}";
+        src = super.fetchFromGitHub {
+          inherit rev;
+          owner = "yt-dlp";
+          repo = "yt-dlp";
+          sha256 = "sha256-u069kH4DsOLwSC7DrXkS0pOSmaYDHd9EwsH/6FirBZI=";
+        };
+      });
   };
   tests = { };
 }
