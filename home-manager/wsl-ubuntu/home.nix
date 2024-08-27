0e682211fcd01f8680d2a2d610cdc21d031f8f58
Chris Warburton <Chris.Warburton@tracsis.com>
2024-08-27 14:50:20 +0100

    wsl-ubuntu: add podman wrapper script
    Containers are nice, but I really dislike the approach taken by Docker and its
    spinoffs. For example, skopeo can upload/download image tarballs to/from
    "registry" servers; whereas the Docker CLI requires a fake "log in", etc.
    
    Haven't yet found a "just run this file" equivalent for running a container
    (runc works; but I don't think it handles layered image manifests like other
    tools support). For now, Podman seems to at least be less obnoxious (I've also
    used Nerdctl and have no real preference between the two).
    
    It seems to run best with sudo, so I've made a wrapper.

--- home-manager/wsl-ubuntu/home.nix
+++ home-manager/wsl-ubuntu/home.nix
@@ -20,6 +20,24 @@
     pkgs.haskellPackages.fourmolu
     (pkgs.hiPrio pkgs.moreutils) # prefer timestamping 'ts' on WSL
 
+    (pkgs.writeShellApplication {
+      # Podman has issues running "rootless", so we just wrap it in sudo. That
+      # needs a little massaging, so (a) it uses our usual $HOME and (b) it has
+      # the required commands on $PATH.
+      name = "podman";
+      runtimeInputs = [
+        pkgs.crun
+        pkgs.podman
+        pkgs.shadow # for newuidmap
+      ];
+      text = ''
+        # We print this message to remind ourselves that we're using this hacky
+        # shell script, when we inevitably encounter un-google-able problems!
+        echo "Running ChrisW's podman sudo wrapper..." 1>&2
+        sudo "HOME=$HOME" "PATH=$PATH" "$(command -v podman)" "$@"
+      '';
+    })
+
     # Fonts
     pkgs.uw-ttyp0
   ];
