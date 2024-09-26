f7abd96f8ef3b5c0ee0ab3ba7e13d5ba69929af8
Chris Warburton <chriswarbo@gmail.com>
2024-09-26 23:21:39 +0100

    nixos-amd64: enable sudo, so we use the wrapper binary

--- nixos/nixos-amd64/configuration.nix
+++ nixos/nixos-amd64/configuration.nix
@@ -87,6 +87,8 @@ with rec {
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  security.sudo.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.chris = {
     isNormalUser = true;
