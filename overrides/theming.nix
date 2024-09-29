3be3a73ab53c33185ce6ca12951d7b546f91e774
Chris Warburton <chriswarbo@gmail.com>
2024-09-29 16:00:38 +0100

    theming: make widgetThemes more robust

--- overrides/theming.nix
+++ overrides/theming.nix
@@ -8,23 +8,31 @@ self: super:
       inherit (self.gnome3) adwaita-icon-theme;
     };
 
-    widgetThemes = {
-      inherit (self)
-        blueshell-theme
-        clearlooks-phenix
-        e17gtk
-        gtk2-aurora-engine
-        gtk_engines
-        gtk-engine-murrine
-        theme-vertex
-        zuki-themes
-        ;
-      skulpture-qt5 = self.skulpture.qt5;
-      skulpture-qt6 = self.skulpture.qt6;
-    };
+    widgetThemes =
+      with rec {
+        warbo-packages = self.warbo-packages or (import ./repos.nix self super).overrides.warbo-packages;
+        skulpture = self.skulpture or warbo-packages.skulpture;
+      }; {
+        # These come from upstream, so should always be available
+        inherit (self)
+          clearlooks-phenix
+          e17gtk
+          gtk_engines
+          gtk-engine-murrine
+          theme-vertex
+          zuki-themes
+          ;
 
-    mkLibsForQt5 =
-      qelf:
-      super.mkLibsForQt5 qelf // { skulpture = qelf.callPackage self.skulpture.mkSkulptureQt5 { }; };
+        # These come from warbo-packages, which may not be included in overlays.
+        # Look them up in self, to allow overrides; but fall back to loading
+        # them directly from warbo-packages.
+        blueshell-theme = self.blueshell-theme or warbo-packages.blueshell-theme;
+
+        # TODO: This is broken due to Nixpkgs renaming pkgconfig
+        #gtk2-aurora-engine = self.gtk2-aurora-engine or warbo-packages.gtk2-aurora-engine
+
+        skulpture-qt5 = self.skulpture-qt5 or skulpture.qt5;
+        skulpture-qt6 = self.skulpture-qt6 or skulpture.qt6;
+      };
   };
 }
