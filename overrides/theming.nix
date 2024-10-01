362d33e8dd51ae26e3133686038c9b6e60d82079
Chris Warburton <chriswarbo@gmail.com>
2024-10-01 18:21:44 +0100

    theming: more robust override

--- overrides/theming.nix
+++ overrides/theming.nix
@@ -1,5 +1,13 @@
 self: super:
 
+with {
+  warbo-packages =
+    self.warbo-packages or (
+      (rec { inherit (import ./repos.nix overrides super) overrides; })
+      .overrides.warbo-packages
+    );
+};
+with { skulpture = self.skulpture or warbo-packages.skulpture; };
 {
   overrides = {
     iconThemes = {
@@ -8,31 +16,28 @@ self: super:
       inherit (self.gnome3) adwaita-icon-theme;
     };
 
-    widgetThemes =
-      with rec {
-        warbo-packages = self.warbo-packages or (import ./repos.nix self super).overrides.warbo-packages;
-        skulpture = self.skulpture or warbo-packages.skulpture;
-      }; {
-        # These come from upstream, so should always be available
-        inherit (self)
-          clearlooks-phenix
-          e17gtk
-          gtk_engines
-          gtk-engine-murrine
-          theme-vertex
-          zuki-themes
-          ;
+    widgetThemes = {
+      # These come from upstream, so should always be available
+      inherit (self)
+        clearlooks-phenix
+        e17gtk
+        gtk_engines
+        gtk-engine-murrine
+        theme-vertex
+        zuki-themes
+        ;
 
-        # These come from warbo-packages, which may not be included in overlays.
-        # Look them up in self, to allow overrides; but fall back to loading
-        # them directly from warbo-packages.
-        blueshell-theme = self.blueshell-theme or warbo-packages.blueshell-theme;
+      # These come from warbo-packages, which may not be included in overlays.
+      # Look them up in self, to allow overrides; but fall back to loading
+      # them directly from warbo-packages.
+      blueshell-theme = self.blueshell-theme or warbo-packages.blueshell-theme;
 
-        # TODO: This is broken due to Nixpkgs renaming pkgconfig
-        #gtk2-aurora-engine = self.gtk2-aurora-engine or warbo-packages.gtk2-aurora-engine
+      # TODO: This is broken due to Nixpkgs renaming pkgconfig
+      #gtk2-aurora-engine = self.gtk2-aurora-engine or
+      #  warbo-packages.gtk2-aurora-engine
 
-        skulpture-qt5 = self.skulpture-qt5 or skulpture.qt5;
-        skulpture-qt6 = self.skulpture-qt6 or skulpture.qt6;
-      };
+      skulpture-qt5 = self.skulpture-qt5 or skulpture.qt5;
+      skulpture-qt6 = self.skulpture-qt6 or skulpture.qt6;
+    };
   };
 }
