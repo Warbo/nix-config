ae98e1e230d6be7b6610aa032788b4bbc096cdef
Chris Warburton <chriswarbo@gmail.com>
2024-10-07 14:57:55 +0100

    chromebook: mount options are bare list now

--- nixos/chromebook/hardware-configuration.nix
+++ nixos/chromebook/hardware-configuration.nix
@@ -41,7 +41,7 @@
   fileSystems."/home/jo/Public" = {
     device = "//dietpi.local/shared";
     fsType = "cifs";
-    options = pkgs.lib.concatStringsSep "," [
+    options = [
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=60"
