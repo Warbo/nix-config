a33d38a7d1760ca6b010e985628f5cb9713d3a1d
Chris Warburton <Chris.Warburton@tracsis.com>
2024-11-01 17:27:15 +0000

    wsl: start a socat tunnel as part of pyselenium

--- home-manager/wsl-ubuntu/pyselenium.nix
+++ home-manager/wsl-ubuntu/pyselenium.nix
@@ -3,10 +3,12 @@ with rec {
   inherit (nix-helpers) nixpkgs;
 };
 { python3 ? nixpkgs.python3
+, socat ? nixpkgs.socat
 , writeShellApplication ? nixpkgs.writeShellApplication
 }:
 writeShellApplication {
   name = "pyselenium";
+  runtimeInputs = [ socat ];
   text = ''
     exec ${python3.withPackages (p: [ p.selenium ])}/bin/python3 \
       ${./pyselenium.py} "$@"
