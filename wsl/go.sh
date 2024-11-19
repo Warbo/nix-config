ed92fcae9a63765180fe1972c751c23e9cefb5c2
Chris Warburton <Chris.Warburton@tracsis.com>
2024-11-19 10:14:58 +0000

    wsl: only try to activate SWAP if it exists

--- wsl/go.sh
+++ wsl/go.sh
@@ -4,10 +4,13 @@
     echo "Going $HOME" 1>&2
     cd
 }
-grep -q SWAP < /proc/swaps || {
-    echo "Activating $PWD/SWAP" 1>&2
-    sudo swapon SWAP
-}
+if [[ -e SWAP ]]
+then
+    grep -q SWAP < /proc/swaps || {
+        echo "Activating $PWD/SWAP" 1>&2
+        sudo swapon SWAP
+    }
+fi
 
 "${LAN:?No LAN script}/bin/lan" || true
 ping -w5 -c1 192.168.0.1 || {
