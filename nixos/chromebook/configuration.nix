0a67b3fd148c134db7d9ba8addf519d5f6039ccb
jo <jo@chromebook>
2025-01-02 23:52:29 +0000

    Avoid some games for now

--- nixos/chromebook/configuration.nix
+++ nixos/chromebook/configuration.nix
@@ -29,7 +29,7 @@
     rclone
     screen
     sshfs
-    transmission-gtk
+    transmission_4-gtk
     unzip
     vlc
     wget
@@ -54,8 +54,8 @@
       ''
     ))
 
-    openmw
-    openra
+    #openmw
+    #openra
     retroarchFull
   ];
 
@@ -201,7 +201,7 @@
   # };
 
   nix = {
-    package = pkgs.nixVersions.nix_2_19; # default of 2.18.1 was corrupt?
+    #package = pkgs.nixVersions.nix_2_19; # default of 2.18.1 was corrupt?
     nixPath = with builtins; [
       "nixos-config=${toString ../..}/nixos/chromebook/configuration.nix"
     ];
