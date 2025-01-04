93e0c6d370aeffbe45b121ce8a5662cdfa480cb4
Chris Warburton <chriswarbo@gmail.com>
2025-01-04 20:34:58 +0000

    yt-dlp: simplify

--- overrides/yt-dlp.nix
+++ overrides/yt-dlp.nix
@@ -1,18 +1,7 @@
-self: super:
-with rec {
-  repos = (import ../overlays.nix).repos (super // repos) super;
-
-  nix-helpers = self.nix-helpers or repos.nix-helpers;
-
-  nixpkgsUpstream = self.nixpkgsUpstream or
-    ((import ../overlays.nix).nixpkgsUpstream
-      (super // nixpkgsUpstream) super).nixpkgsUpstream;
-};
-{
+self: super: {
   overrides = {
     yt-dlp =
       with {
-        inherit (nix-helpers.pinnedNixpkgs.nixpkgs2111) phantomjs2;
         src = super.fetchFromGitHub {
           owner = "yt-dlp";
           repo = "yt-dlp";
@@ -20,13 +9,9 @@ with rec {
           sha256 = "sha256-6zv2NqbUxirMwa6OCzeKXuqLB4sFiYAcy3TYke2jhKc=";
         };
       };
-      nixpkgsUpstream.yt-dlp.overrideAttrs (old: {
+      super.yt-dlp.overrideAttrs (old: {
         inherit src;
         name = "yt-dlp-${src.rev}";
-        postInstall = ''
-          ${old.postInstall or ""}
-          wrapProgram "$out/bin/yt-dlp" --prefix PATH : "${phantomjs2}/bin"
-        '';
       });
   };
   tests = { };
