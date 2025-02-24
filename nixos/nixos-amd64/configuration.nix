87b66e9c140f901f1c0db8e02b784d107edfe05c
Chris Warburton <chriswarbo@gmail.com>
2025-02-24 22:44:09 +0000

    nixos-amd64: enable ollama

--- nixos/nixos-amd64/configuration.nix
+++ nixos/nixos-amd64/configuration.nix
@@ -191,6 +191,10 @@
     settings.Addresses.API = [ "/ip4/127.0.0.1/tcp/5001" ];
   };
 
+  services.ollama = {
+    enable = true;
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
