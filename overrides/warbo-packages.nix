2249db87fd039e1a65136521610030476d751786
Chris Warburton <chriswarbo@gmail.com>
2025-03-28 01:30:41 +0000

    Unwrap nix-helpers and warbo-packages
    Rather than our overlays giving us 'pkgs.nix-helpers' and 'pkgs.warbo-packages',
    we want all of their contents to get added; like 'pkgs.feed2maildirsimple', etc.
    
    This is actually quite tricky, since the *spine* of the pkgs attrset is strict,
    which can cause infinite recursion. We get around this by exploiting the fact
    that the attribute names provided by both nix-helpers and warbo-packages do not
    depend on the particular nixpkgs argument they're given. Hence we can populate
    the spine of 'pkgs' using names taken from those repos applied to a nixpkgs that
    doesn't contain any reference to them; but associate those names to values taken
    from those repos applied to a nixpkgs that *does* contain references to them.

--- overrides/warbo-packages.nix
+++ overrides/warbo-packages.nix
@@ -1,8 +1,30 @@
+with {
+  # If we've not got warbo-packages already, we'll get it from warbo-utilities.
+  warbo-utilities = self: super:
+    (import ./warbo-utilities.nix self super).overrides.warbo-utilities;
+};
 self: super:
+with rec {
+  inherit (builtins) mapAttrs;
+
+  # These are the packages we want to use since they take dependencies from self
+  # and hence use any overlays that are in effect. However, we can't use them to
+  # get our attribute names, since that can cause infinite recursion.
+  recursive = (warbo-utilities self super).warbo-packages;
+
+  # This avoids infinite recursion and hence can be used to get attribute names.
+  # However, their values should be discarded since they ignore any overlays.
+  nonrecursive = (warbo-utilities nixpkgs nixpkgs).warbo-packages;
+
+  # Override the default pinned nixpkgs with ours (if we have one), but don't
+  # load any overlays in order to avoid infinite recursion.
+  nixpkgs =
+    if super ? path
+    then import super.path { config = {}; overlays = []; }
+    else {};
+};
 {
-  overrides.warbo-packages =
-    super.warbo-packages or
-      (super.warbo-utilities.warbo-packages or
-        ((import ./warbo-utilities.nix self super)
-        .overrides.warbo-utilities.warbo-packages));
+  overrides = super.warbo-packages or
+    super.warbo-utilities.warbo-packages or
+    (mapAttrs (n: _: recursive.${n}) nonrecursive);
 }
