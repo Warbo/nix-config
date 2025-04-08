11c4d2d45a806d5125e704be9f3d1bf490d7d1ac
Chris Warburton <chriswarbo@gmail.com>
2025-04-08 15:15:16 +0100

    fetch-news: add dummy description to items that don't have one
    This avoids an error in feed2maildir

--- nixos/modules/unsummarise.py
+++ nixos/modules/unsummarise.py
@@ -14,4 +14,20 @@ rss = feeds.parse(data, url=url, encoding=enc)
 rss = rss.convert(feeds.FeedXML)
 rss = FeedGather(rss, url, options)
 out = FeedFormat(rss, options, 'unicode')
-print(out)
+
+# Add <description> to items, if they don't already have one
+import xml.etree.ElementTree as ET
+
+root = ET.fromstring(out)
+for item in root.findall('.//item'):
+    if item.find('description') is None:
+        # Find the link text
+        link_elem = item.find('link')
+        link_text = link_elem.text if link_elem is not None and link_elem.text \
+            else "No description"
+
+        # Create and add description element
+        description = ET.SubElement(item, 'description')
+        description.text = f"Link: {link_text}"
+
+print(ET.tostring(root, encoding='unicode'))
