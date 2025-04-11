df9bcfeeca5f60723c45511f75e6ddd626346a86
Chris Warburton <chriswarbo@gmail.com>
2025-04-11 14:21:57 +0100

    Remove overlay-nix-store and with-nix-store commands
    They're not needed; better to offload work on to other machines, use them as
    remote stores, and aggressively garbage-collect.

--- home-manager/commands/overlay-nix-store/default.nix
+++
@@ -1,7 +0,0 @@
-{ mergerfs, writeShellApplication }:
-
-writeShellApplication {
-  name = "overlay-nix-store";
-  runtimeInputs = [ mergerfs ];
-  text = builtins.readFile ./overlay-nix-store.sh;
-}
