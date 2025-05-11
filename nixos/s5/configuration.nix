c9071fa97ae8c89727f1558c16d8d4ab7c415f34
Chris Warburton <chriswarbo@gmail.com>
2025-05-11 17:28:27 +0100

    s5: Try disabling some binfmt options that give assertion failures

--- nixos/s5/configuration.nix
+++ nixos/s5/configuration.nix
@@ -35,15 +35,19 @@ with {
         "aarch64-linux" # Pinephone
         "armv6l-linux" # RaspberryPi
         "i686-linux" # Thinkpad
-        "riscv64-linux" # VisionFive
+        #"riscv64-linux" # VisionFive
         "x86_64-linux" # Laptops
       ];
     # https://github.com/felixonmars/archriscv-packages/blob/7c270ecef6a84edd6031b357b7bd1f6be2d6d838/devtools-riscv64/z-archriscv-qemu-riscv64.conf#L1
-    registrations."x86_64-linux" = {
+    /*registrations."x86_64-linux" = {
       preserveArgvZero = true;
       matchCredentials = true;
-      fixBinary = true;
-    };
+      # TODO: 2025-05-11: Comment-out to avoid assertion failure that "cannot
+      # have fixBinary when the interpreter is invoked through a shell". Note
+      # that this failure doesn't occur when running INSTALL, but does in e.g. a
+      # nix-repl session.
+      #fixBinary = true;
+    };*/
   };
 
   system.tools.nixos-option.enable = false;  # This drags in an old Nix 2.18
