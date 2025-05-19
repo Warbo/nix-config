7a48101f5c63dc71bf056c2852cd490b892820b3
Chris Warburton <chris.warburton@tracsis.com>
2025-05-19 14:04:28 +0100

    nixos-basic: propagate NIX_PATH

--- nixos/nixos-basic/update.sh
+++ nixos/nixos-basic/update.sh
@@ -6,7 +6,7 @@ NIXOS_PATH=$(nix-instantiate --eval --expr '<nixpkgs/nixos>')
 
 # Pass $PATH through to sudo, since root doesn't have our Nix profile
 # Also pass SSH_AUTH_SOCK, since root's SSH access is different to our user's
-sudo env PATH="$PATH" SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
+sudo env PATH="$PATH" SSH_AUTH_SOCK="$SSH_AUTH_SOCK" NIX_PATH="$NIX_PATH" \
      nixos-container update nixos-basic \
      --config-file configuration.nix \
      --nixos-path "$NIXOS_PATH"
