51c7758d7ff9e2582f866e720e675ce334806213
Chris Warburton <chris.warburton@tracsis.com>
2025-06-05 17:31:45 +0100

    nixos-basic: rely on wsl-ubuntu's nixos-container wrapper

--- nixos/nixos-basic/update.sh
+++ nixos/nixos-basic/update.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # Update nixos-basic container using this configuration.nix
+set -e
 
-# Figure out NixOS path
 NIXOS_PATH=$(nix-instantiate --eval --expr '<nixpkgs/nixos>')
 
-# Pass $PATH through to sudo, since root doesn't have our Nix profile
-# Also pass SSH_AUTH_SOCK, since root's SSH access is different to our user's
-sudo env PATH="$PATH" SSH_AUTH_SOCK="$SSH_AUTH_SOCK" NIX_PATH="$NIX_PATH" \
-     nixos-container update nixos-basic \
-     --config-file configuration.nix \
-     --nixos-path "$NIXOS_PATH"
+# Relies on our nixos-container wrapper, which calls sudo and propagates a
+# bunch of env vars!
+nixos-container update nixos-basic \
+                --config-file configuration.nix \
+                --nixos-path "$NIXOS_PATH"
