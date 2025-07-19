3e8fc8019023981dede1c8e43f8370639afbd59c
Chris Warburton <chriswarbo@gmail.com>
2025-07-19 11:05:20 +0100

    Bump warbo-utilities

--- overrides/warbo-utilities.nix
+++ overrides/warbo-utilities.nix
@@ -7,7 +7,7 @@ with rec {
     .overrides.fetchGitIPFS;
 
   warbo-utilities-src = fetchGitIPFS {
-    sha1 = "98658033d67546655a0fcf843e5ebb0bcc6340b9";
+    sha1 = "74e84a683b7180008f1e528cbee636ee4eef8c03";
   };
 
   get = name: { ${if hasAttr name super then name else null} = super.${name}; };
